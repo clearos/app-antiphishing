95f270d2140cf3b66672f6f728a6f351766628c1
Ben Chambers <bchambers@clearfoundation.com>
2012-01-11 16:25:36 +0000

    - Updated description

--- language/en_US/antiphishing_lang.php
+++ language/en_US/antiphishing_lang.php
@@ -1,7 +1,7 @@
 <?php
 
 # -*- coding: utf-8 -*-
-$lang['antiphishing_app_description'] = 'Gateway Antiphishing provides protection from your network and server.';
+$lang['antiphishing_app_description'] = 'Gateway Antiphishing uses a central antiphishing engine to scan web traffic and links. It protects devices connected to your network by preventing users from following malicious links if a positive match is made against a database of known phishing signatures.';
 $lang['antiphishing_app_name'] = 'Gateway Antiphishing';
 $lang['antiphishing_app_tooltip'] = 'Even the most savvy Internet users can accidentally click on a phishing link.  We recommend leaving all antiphishing features enabled.';
 $lang['antiphishing_block_cloaked'] = 'Block Cloaked URLs';
