e2dd5163e312ff534890006ac166e19eb46b8d88
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-04-19 00:42:30 +0000

    - Harmonized app description

--- language/en_US/antiphishing_lang.php
+++ language/en_US/antiphishing_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'Gateway Antiphishing uses a central antiphishing engine to scan web, FTP, mail and more. It protects devices connected to your network by shielding users from malicious links detected by the engine.';
+$lang['antiphishing_app_description'] = 'The Gateway Antiphishing app uses a central antiphishing engine to scan web, FTP, mail and more. It protects devices connected to your network by shielding users from malicious links detected by the engine.';
 $lang['antiphishing_app_name'] = 'Gateway Antiphishing';
 $lang['antiphishing_app_tooltip'] = 'Even the most savvy Internet users can accidentally click on a phishing link.  We recommend leaving all antiphishing features enabled.';
 $lang['antiphishing_block_cloaked'] = 'Block Cloaked URLs';
