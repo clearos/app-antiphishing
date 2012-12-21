9c1a827648de36bae33585eab4f3ba5ae2bd51c1
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-12-21 23:05:39 +0000

    - Updated translations

--- language/pt_BR/antiphishing_lang.php
+++ language/pt_BR/antiphishing_lang.php
@@ -3,9 +3,9 @@
 // http://www.clearfoundation.com/docs/developer/translations/
 
 $lang['antiphishing_app_description'] = 'The Gateway Antiphishing app uses a central antiphishing engine to scan web, FTP, mail and more. It protects devices connected to your network by shielding users from malicious links detected by the engine.';
-$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'Even the most savvy Internet users can accidentally click on a phishing link.  We recommend leaving all antiphishing features enabled.';
-$lang['antiphishing_block_cloaked'] = 'Block Cloaked URLs';
-$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL Mismatch';
-$lang['antiphishing_heuristics_engine'] = 'Heuristic Engine';
-$lang['antiphishing_signature_engine'] = 'Signature Engine';
+$lang['antiphishing_app_name'] = 'Anti-Pishing Gateway';
+$lang['antiphishing_app_tooltip'] = 'Até mesmo os usuários mais experientes podem acidentalmente clicar em um link de phishing. Recomendamos manter todos os recursos anti-phishing ativos.';
+$lang['antiphishing_block_cloaked'] = 'Bloquear URLs camufladas';
+$lang['antiphishing_block_ssl_mismatch'] = 'Bloquear SSL incompatível';
+$lang['antiphishing_heuristics_engine'] = 'Mecanismo Herístico';
+$lang['antiphishing_signature_engine'] = 'Mecanismo de Assinatura';
