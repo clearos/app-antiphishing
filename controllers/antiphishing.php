afcef4b9a8d6619c0e4222d2d5c32d2aaa0e205a
Peter Baldwin <pbaldwin@clearfoundation.com>
2013-05-22 19:34:25 +0000

    - Cleaned up @category, @package, and @subpackage for phpDoc generation

--- controllers/antiphishing.php
+++ controllers/antiphishing.php
@@ -3,9 +3,9 @@
 /**
  * Antiphishing controller.
  *
- * @category   Apps
- * @package    Antiphishing
- * @subpackage Controllers
+ * @category   apps
+ * @package    antiphishing
+ * @subpackage controllers
  * @author     ClearFoundation <developer@clearfoundation.com>
  * @copyright  2011 ClearFoundation
  * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License version 3 or later
@@ -36,9 +36,9 @@
 /**
  * Antiphishing controller.
  *
- * @category   Apps
- * @package    Antiphishing
- * @subpackage Controllers
+ * @category   apps
+ * @package    antiphishing
+ * @subpackage controllers
  * @author     ClearFoundation <developer@clearfoundation.com>
  * @copyright  2011 ClearFoundation
  * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License version 3 or later
--- views/antiphishing.php
+++ views/antiphishing.php
@@ -3,9 +3,9 @@
 /**
  * Antiphishing view.
  *
- * @category   ClearOS
- * @package    Antiphishing
- * @subpackage Views
+ * @category   apps
+ * @package    antiphishing
+ * @subpackage views
  * @author     ClearFoundation <developer@clearfoundation.com>
  * @copyright  2011 ClearFoundation
  * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License version 3 or later
