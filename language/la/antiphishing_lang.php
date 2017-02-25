5df0903543d095b2bb3e15a3bd1f6ca8ced4d354
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:54:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'Praesent Antiphishing App Engine et scan in telam ponit medium Antiphishing, FTP, et mail. Quod protegit tua fabrica coniunctum network users per protegens de Engine maligna links deprehenditur.';
+$lang['antiphishing_app_name'] = 'Antiphishing Gateway';
+$lang['antiphishing_app_tooltip'] = 'Sed savvy maxime Internet users click per accidens, non potest in links a phishing. We recommend relictis omnibus features Antiphishing enabled.';
+$lang['antiphishing_block_cloaked'] = 'Block Cloaked URLs';
+$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL Mismatch';
+$lang['antiphishing_heuristics_engine'] = 'Vivamus heuristic';
+$lang['antiphishing_signature_engine'] = 'signature Engine';
