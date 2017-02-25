27e0d0aebd38f1bf578c05612c08381f339af2d4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:54:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'ການ app Gateway Antiphishing ນໍາໃຊ້ເຄື່ອງຈັກໃນການ antiphishing ສູນກາງການສະແກນເວັບໄຊທ໌ FTP, mail ແລະຫຼາຍ. ມັນປົກປ້ອງອຸປະກອນເຊື່ອມຕໍ່ກັບເຄືອຂ່າຍຂອງທ່ານໂດຍການປ້ອງກັນຜູ້ໃຊ້ຈາກການເຊື່ອມຕໍ່ເປັນອັນຕະລາຍທີ່ກວດພົບໂດຍເຄື່ອງຈັກໃນການ.';
+$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'ເຖິງແມ່ນວ່າຜູ້ໃຊ້ອິນເຕີເນັດ savvy ຫຼາຍທີ່ສຸດອຸບັດຕິເຫດສາມາດຄລິກໃສ່ການເຊື່ອມຕໍ່ phishing ໄດ້. ພວກເຮົາແນະນໍາໃຫ້ເຮັດໃຫ້ຄຸນນະສົມບັດ antiphishing ທັງຫມົດເປີດການໃຊ້ງານ.';
+$lang['antiphishing_block_cloaked'] = 'ທີ່ຢູ່ເວັບໄຊ Block Cloaked';
+$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL ບໍ່ກົງກັນ';
+$lang['antiphishing_heuristics_engine'] = 'Engine heuristic';
+$lang['antiphishing_signature_engine'] = 'Engine ລາຍເຊັນ';
