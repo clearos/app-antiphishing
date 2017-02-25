d1babd07d9e85f0819739fca89cdbdc205975a19
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:53:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'The Gateway antiphishing aplikasi menggunakan mesin antiphishing pusat untuk memindai web, FTP, mail dan banyak lagi. Ini melindungi perangkat yang terhubung ke jaringan Anda dengan melindungi pengguna dari link berbahaya terdeteksi oleh mesin.';
+$lang['antiphishing_app_name'] = 'Gateway antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Bahkan pengguna internet yang paling cerdas sengaja dapat mengklik link phishing. Sebaiknya meninggalkan semua fitur antiphishing diaktifkan.';
+$lang['antiphishing_block_cloaked'] = 'Blok berjubah URL';
+$lang['antiphishing_block_ssl_mismatch'] = 'Blok SSL Mismatch';
+$lang['antiphishing_heuristics_engine'] = 'Mesin heuristik';
+$lang['antiphishing_signature_engine'] = 'Signature Mesin';
