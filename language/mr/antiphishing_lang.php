c536b3719b9fa3f21e573caa5d26f312b695c49d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:55:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'गेटवे Antiphishing अॅप वेब, FTP, मेल आणि अधिक स्कॅन एक मध्यवर्ती antiphishing इंजिन वापरते. हे इंजिन आढळले दुर्भावनायुक्त दुवे वापरकर्त्यांना संरक्षण करून आपल्या नेटवर्क कनेक्ट केलेले साधने संरक्षण होते.';
+$lang['antiphishing_app_name'] = 'गेटवे Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'अगदी सर्वात जाणकार इंटरनेट वापरकर्त्यांसाठी चुकून फिशिंग लिंक वर क्लिक करा शकता. आम्ही सक्षम सर्व antiphishing वैशिष्ट्ये सोडून शिफारस करतो.';
+$lang['antiphishing_block_cloaked'] = 'ब्लॉक फसवे केलेल्या URL';
+$lang['antiphishing_block_ssl_mismatch'] = 'ब्लॉक SSL जुळत नाही';
+$lang['antiphishing_heuristics_engine'] = 'नवगामी इंजिन';
+$lang['antiphishing_signature_engine'] = 'स्वाक्षरी इंजिन';
