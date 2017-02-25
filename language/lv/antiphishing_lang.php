38a3fa3c452909632628c8fe8e2b04d7c8eda804
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:55:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'Gateway Antiphishing lietotne izmanto centrālā antiphishing dzinēju, lai skenētu mājas, FTP, pasta un vairāk. Tas aizsargā ierīces, kas saistīts ar jūsu tīklu, pasargājot lietotājus no ļaunprātīgas saites atklāto motora.';
+$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Pat visvairāk savvy interneta lietotāji var nejauši noklikšķināt uz pikšķerēšanas saites. Mēs iesakām atstājot visus Antiphishing iezīmes ļāva.';
+$lang['antiphishing_block_cloaked'] = 'Bloķēt Cloaked URL';
+$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL neatbilstība';
+$lang['antiphishing_heuristics_engine'] = 'heiristisko Dzinējs';
+$lang['antiphishing_signature_engine'] = 'paraksts Dzinējs';
