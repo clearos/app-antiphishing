633043e816b34ee06d11f51c6d88e901a4221713
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:53:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'A Gateway Adathalászat app használ központi adathalászat motor olvasni web, FTP, e-mail és így tovább. Védi eszközök a hálózathoz csatlakoztatott árnyékolással felhasználókat a rosszindulatú hivatkozások által érzékelt motor.';
+$lang['antiphishing_app_name'] = 'Gateway adathalászat';
+$lang['antiphishing_app_tooltip'] = 'Még a legnagyobb hozzáértés internet felhasználók véletlenül kattintva egy adathalász linket. Javasoljuk, hogy hagyja az összes adathalászat funkciók engedélyezve.';
+$lang['antiphishing_block_cloaked'] = 'Blokk bújtatott URL';
+$lang['antiphishing_block_ssl_mismatch'] = 'Blokk SSL nem egyezik';
+$lang['antiphishing_heuristics_engine'] = 'heurisztikus motor';
+$lang['antiphishing_signature_engine'] = 'Signature Engine';
