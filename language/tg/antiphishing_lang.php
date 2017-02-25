4abe0d02bba846489cd34188786cc0fa3bdccbc0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:59:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'Дар барнома Gateway Antiphishing мебарад муҳаррики antiphishing марказии ба скан веб, FTP, почтаи электронӣ ва бештар. Ин дастгоҳҳо бо муҳофизат аз истифодабарандагони пайвандҳо шубҳанок бошанд, ки ошкор муҳаррики ба шабакаи худ пайваст, муҳофизат мекунад.';
+$lang['antiphishing_app_name'] = 'дарвоза Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Ҳатто истифодабарандагони Интернет идрок ҳама тасодуфан метавонад дар пайванди фишинг пахш кунед. Мо тавсия тарк ҳамаи хусусиятҳои antiphishing фаъол.';
+$lang['antiphishing_block_cloaked'] = 'Блоки қафои URL-';
+$lang['antiphishing_block_ssl_mismatch'] = 'Блоки SSL номусоидии';
+$lang['antiphishing_heuristics_engine'] = 'Engine Heuristic';
+$lang['antiphishing_signature_engine'] = 'имзо Engine';
