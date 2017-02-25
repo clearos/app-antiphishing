4704bf5d26d672ec8d5ed93fc13a2fa649d23306
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:59:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'แอปพลิเคเกตเวย์ Antiphishing ใช้เครื่องยนต์ antiphishing กลางที่จะสแกนเว็บ, FTP, อีเมลและอื่น ๆ จะช่วยปกป้องอุปกรณ์ที่เชื่อมต่อกับเครือข่ายของคุณโดยการป้องกันผู้ใช้จากลิงก์ที่เป็นอันตรายที่ตรวจพบโดยเครื่องยนต์';
+$lang['antiphishing_app_name'] = 'เกตเวย์ Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'แม้ผู้ใช้อินเทอร์เน็ตเข้าใจมากที่สุดตั้งใจสามารถคลิกที่ลิงค์ฟิชชิ่ง เราขอแนะนำให้ออกจากคุณลักษณะ antiphishing เปิดใช้งานทั้งหมด';
+$lang['antiphishing_block_cloaked'] = 'บล็อก URL ปิดบัง';
+$lang['antiphishing_block_ssl_mismatch'] = 'บล็อก SSL ไม่ตรงกัน';
+$lang['antiphishing_heuristics_engine'] = 'เครื่องยนต์ Heuristic';
+$lang['antiphishing_signature_engine'] = 'เครื่องยนต์ลายเซ็น';
