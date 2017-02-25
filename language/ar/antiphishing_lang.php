ec453d7d22fb2c6452d8aca8578530d8044e42e9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:49:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'يستخدم التطبيق بوابة تصيد محرك تصيد المركزي لمسح على شبكة الإنترنت، بروتوكول نقل الملفات، والبريد، وأكثر من ذلك. لأنه يحمي الأجهزة المتصلة بالشبكة عن طريق حجب المستخدمين من الروابط الخبيثة الكشف عنها بواسطة محرك.';
+$lang['antiphishing_app_name'] = 'بوابة تصيد';
+$lang['antiphishing_app_tooltip'] = 'يمكن حتى مستخدمي الإنترنت الأكثر تطورا انقر فوق بطريق الخطأ على وجود صلة التصيد. ونوصي بترك كافة الميزات تصيد تمكين.';
+$lang['antiphishing_block_cloaked'] = 'كتلة غطى عناوين';
+$lang['antiphishing_block_ssl_mismatch'] = 'كتلة SSL عدم تطابق';
+$lang['antiphishing_heuristics_engine'] = 'محرك ارشادي';
+$lang['antiphishing_signature_engine'] = 'التوقيع المحرك';
