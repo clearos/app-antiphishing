fa188373b2ba1123a16e1eb7606f761ba90e917f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:55:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'ഗേറ്റ്വേ Antiphishing അപ്ലിക്കേഷൻ വെബ്, എഫ്ടിപി, മെയിൽ കൂടുതൽ സ്കാൻ ചെയ്യാൻ കേന്ദ്ര antiphishing എഞ്ചിൻ ഉപയോഗിക്കുന്നു. ഇത് എൻജിൻ കണ്ടെത്തി ക്ഷുദ്ര ലിങ്കുകൾ നിന്നും ഉപയോക്താക്കളെ shielding വഴി നിങ്ങളുടെ നെറ്റ്വർക്കിൽ കണക്ട് ഡിവൈസുകൾ സംരക്ഷിക്കുന്നു.';
+$lang['antiphishing_app_name'] = 'ഗേറ്റ്വേ Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'ഏറ്റവും അരച്ചുകലക്കി ഇന്റർനെറ്റ് ഉപയോക്താക്കൾ പോലും അബദ്ധത്തിൽ ഫിഷിംഗ് ലിങ്കിൽ ക്ലിക്ക് ചെയ്യാം. നാം പ്രാപ്തമാക്കിയ എല്ലാ antiphishing സവിശേഷതകൾ വിട്ടുകൊടുത്തത് ശുപാർശ.';
+$lang['antiphishing_block_cloaked'] = 'ബ്ലോക്ക് മറഞ്ഞു URL- കൾ';
+$lang['antiphishing_block_ssl_mismatch'] = 'ബ്ലോക്ക് SSL പൊരുത്തക്കേട്';
+$lang['antiphishing_heuristics_engine'] = 'ഹ്യുറിസ്റ്റിക് എഞ്ചിൻ';
+$lang['antiphishing_signature_engine'] = 'ഒപ്പ് എഞ്ചിൻ';
