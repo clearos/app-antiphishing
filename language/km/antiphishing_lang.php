4f5db85479e32b08b989194b9c826a287021b9ad
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:54:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'កម្មវិធីប្រើម៉ាស៊ីន Antiphishing ច្រកផ្លូវកណ្តាលដើម្បី antiphishing ស្កេនបណ្ដាញ, FTP របស់សំបុត្រនិងច្រើនទៀត។ វាការពារឧបករណ៍ភ្ជាប់បណ្តាញរបស់អ្នកដោយការការពារអ្នកប្រើប្រាស់ពីតំណភ្ជាប់ដែលមានគំនិតអាក្រក់បានរកឃើញដោយម៉ាស៊ីន។';
+$lang['antiphishing_app_name'] = 'ផ្លូវចេញចូល Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'ទោះបីជាអ្នកប្រើប្រាស់អ៊ីនធើណែបទពិសោធច្រើនបំផុតចៃដន្យអាចចុចលើតំណបន្លំ។ យើងសូមណែនាំឱ្យចាកចេញពីលក្ខណៈពិសេសទាំងអស់បានបើក antiphishing ។';
+$lang['antiphishing_block_cloaked'] = 'ប្លុក cloaked URLs ដែលបាន';
+$lang['antiphishing_block_ssl_mismatch'] = 'ប្លុក SSL ដែលមិនផ្គូផ្គង';
+$lang['antiphishing_heuristics_engine'] = 'ម៉ាស៊ីន heuristic';
+$lang['antiphishing_signature_engine'] = 'ម៉ាស៊ីនហត្ថលេខា';
