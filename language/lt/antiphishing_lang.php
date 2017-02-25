7c76dcb0a57c03313f54408782b514114ff5fc10
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:54:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'Vartai Kovotojai su nelegaliu programa naudoja centrinę Kovotojai su nelegaliu programą nuskaityti interneto, FTP, paštą ir daugiau. Jis apsaugo prietaisus prijungti prie tinklo ekranavimas vartotojus nuo kenkėjiškų nuorodų aptiktų variklio.';
+$lang['antiphishing_app_name'] = 'vartai Kovotojai su nelegaliu';
+$lang['antiphishing_app_tooltip'] = 'Net labiausiai patyrusių interneto vartotojų gali netyčia paspausti ant phishing nuorodą. Mes rekomenduojame palikti visas Kovotojai su nelegaliu funkcijų aktyvuota.';
+$lang['antiphishing_block_cloaked'] = 'Blokuoti Paslėptas URL';
+$lang['antiphishing_block_ssl_mismatch'] = 'Blokuoti SSL neatitikimas';
+$lang['antiphishing_heuristics_engine'] = 'euristinis Variklis';
+$lang['antiphishing_signature_engine'] = 'Parašas Variklis';
