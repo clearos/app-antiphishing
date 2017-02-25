2ffd5c12dc42edfdebf1b396679e5224e7de5e6b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:00:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'Mae&#39;r app Porth Antiphishing defnyddio peiriant antiphishing ganolog i sganio y we, FTP, post a mwy. Mae&#39;n amddiffyn dyfeisiau sy&#39;n gysylltiedig â&#39;ch rhwydwaith drwy gysgodi defnyddwyr o gysylltiadau maleisus a ganfuwyd gan y peiriant.';
+$lang['antiphishing_app_name'] = 'Porth Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Gall hyd yn oed y defnyddwyr y Rhyngrwyd mwyaf savvy ddamweiniol glicio ar ddolen gwe-rwydo. Rydym yn argymell gadael holl nodweddion antiphishing alluogi.';
+$lang['antiphishing_block_cloaked'] = 'URLs Bloc y Sbriws';
+$lang['antiphishing_block_ssl_mismatch'] = 'Bloc SSL Camgymhariad';
+$lang['antiphishing_heuristics_engine'] = 'Engine hewristig';
+$lang['antiphishing_signature_engine'] = 'Engine Llofnod';
