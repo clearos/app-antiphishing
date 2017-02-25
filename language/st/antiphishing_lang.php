016edb15f50da4cf70d1d93382e4b7aa0b1bc886
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:58:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'The app e heke Antiphishing sebelisa bohareng antiphishing enjene go Scan web, FTP, mangolo le ho feta. E sireletsa disebediswa tse amanang le ho hokahanngoa hao ka sireletsoe basebedisi ho tloha Les liens de le nang le lonya lemoha ka enjene.';
+$lang['antiphishing_app_name'] = 'heke Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Esita le basebedisi fetisisa savvy Internet ka tobetsa ka phoso mabapi le lehokela phishing. Re ho kgothaletsa siea litšobotsi tsohle antiphishing nolofalletsa.';
+$lang['antiphishing_block_cloaked'] = 'Block Cloaked URLs';
+$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL Mismatch';
+$lang['antiphishing_heuristics_engine'] = 'Heuristic Engine';
+$lang['antiphishing_signature_engine'] = 'Signature Engine';
