02980ec6908fa085ec4bf27bbd8734f86e2dc6ce
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:49:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'Gateway Antiphishing Aplikazioa central antiphishing motor bat erabiltzen du web, FTP, mail eta gehiago eskaneatu. Zure sarera konektatuta maltzurren loturak motorra detektatu erabiltzaile estali by gailu babesten du.';
+$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Nahiz eta gehien savvy internauten daiteke ustekabean phishing esteka baten gainean klik egin. gaituta antiphishing ezaugarri guztiak utziz gomendatzen dugu.';
+$lang['antiphishing_block_cloaked'] = 'Block Estalita URLak';
+$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL datoz';
+$lang['antiphishing_heuristics_engine'] = 'heuristikoak Engine';
+$lang['antiphishing_signature_engine'] = 'Sinadura Engine';
