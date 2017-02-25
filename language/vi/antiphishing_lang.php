552c2eb1e2a6bc78adb572a063c9310f5d458517
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:00:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'Ứng dụng Cổng Antiphishing sử dụng một động cơ chống giả mạo trung tâm để quét web, FTP, mail và nhiều hơn nữa. Nó bảo vệ các thiết bị kết nối vào mạng của bạn bằng cách chặn người dùng từ các liên kết độc hại được phát hiện bởi các công cụ.';
+$lang['antiphishing_app_name'] = 'cổng Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Ngay cả những người sử dụng Internet hiểu biết nhất có thể vô tình bấm vào một liên kết lừa đảo. Chúng tôi khuyên bạn nên để lại tất cả các tính năng chống phishing được kích hoạt.';
+$lang['antiphishing_block_cloaked'] = 'Chặn URL Trùm kín';
+$lang['antiphishing_block_ssl_mismatch'] = 'Khối SSL không phù hợp';
+$lang['antiphishing_heuristics_engine'] = 'Động cơ Heuristic';
+$lang['antiphishing_signature_engine'] = 'Chữ ký cơ';
