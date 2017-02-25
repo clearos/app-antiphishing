5eafd5935472597844e1e8834ea44ffdf1b8c612
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:51:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'Brána Antiphishing aplikace používá centrální phishingem engine pro skenování web, FTP, pošty a další. Chrání zařízení připojená k síti tím, že chrání uživatele před škodlivými odkazy zjištěných motoru.';
+$lang['antiphishing_app_name'] = 'brána Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Dokonce i ty zdatní uživatelé internetu mohou náhodně kliknout na odkaz phishing. Doporučujeme ponechat všechny funkce AntiPhishing povolena.';
+$lang['antiphishing_block_cloaked'] = 'Blokové Maskovaná URL';
+$lang['antiphishing_block_ssl_mismatch'] = 'Blok SSL Neshoda';
+$lang['antiphishing_heuristics_engine'] = 'heuristická Engine';
+$lang['antiphishing_signature_engine'] = 'podpis Engine';
