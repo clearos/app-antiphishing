80f1d2743f7e8aad797363bd5f16d8ade2e77e7b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:53:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'The Gateway Antiphishing app notar miðlæga antiphishing vél til að skanna vefinn, FTP, tölvupóst og fleira. Það ver tæki tengd við netið til að hlífa notendur frá illgjarn tenglar uppgötva við the vél.';
+$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Jafnvel kunnátta notandi getur tilviljun smellt á phishing tengilinn. Við mælum með að fara alla antiphishing eiginleika virka.';
+$lang['antiphishing_block_cloaked'] = 'Blokk Cloaked vefslóðir';
+$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL Misræmi';
+$lang['antiphishing_heuristics_engine'] = 'leitandi Engine';
+$lang['antiphishing_signature_engine'] = 'undirskrift Engine';
