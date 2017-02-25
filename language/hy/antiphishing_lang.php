e047f2fe4183e3283ee17f20fa823734b4b29a95
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:49:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'The Gateway AntiPhishing ծրագիրը օգտագործում է կենտրոնական AntiPhishing շարժիչը սկան վեբ, FTP, փոստով եւ այլն: Այն պաշտպանում է սարքերի հետ կապված ձեր ցանցի պաշտպանելով օգտվողներին չարակամ հղումներ կողմից հայտնաբերված շարժիչը:';
+$lang['antiphishing_app_name'] = 'Gateway AntiPhishing';
+$lang['antiphishing_app_tooltip'] = 'Նույնիսկ առավել ըմբռնողություն Ինտերնետից օգտվողները կարող են պատահաբար սեղմել է phishing հղումը: Մենք խորհուրդ ենք տալիս թողնելով բոլոր AntiPhishing հատկանիշները միացված է:';
+$lang['antiphishing_block_cloaked'] = 'Արգելափակման cloaked URL- ները';
+$lang['antiphishing_block_ssl_mismatch'] = 'Արգելափակել SSL անհամապատասխանություն';
+$lang['antiphishing_heuristics_engine'] = 'heuristic Engine';
+$lang['antiphishing_signature_engine'] = 'Signature Engine';
