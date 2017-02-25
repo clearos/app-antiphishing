025b651048623072141cd61f9b7079ac18b56fdc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:50:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/be/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'Дадатак Шлюз фішынгу выкарыстоўвае цэнтральны антыспам рухавік для сканавання Web, FTP, пошту і многае іншае. Ён абараняе прылады, падлучаныя да сеткі, абараняючы карыстальнікаў ад выяўленых шкоднасных спасылак рухавіка.';
+$lang['antiphishing_app_name'] = 'шлюз Антифишинговая';
+$lang['antiphishing_app_tooltip'] = 'Нават самыя дасведчаныя карыстальнікі Інтэрнэту могуць выпадкова націснуць на спасылку фішынг. Мы рэкамендуем пакінуць усе функцыі AntiPhishing ўключаны.';
+$lang['antiphishing_block_cloaked'] = 'Блок спасылак Схаваная';
+$lang['antiphishing_block_ssl_mismatch'] = 'Блок SSL Несупадзенне';
+$lang['antiphishing_heuristics_engine'] = 'эўрыстычны Рухавічок';
+$lang['antiphishing_signature_engine'] = 'подпіс рухавіка';
