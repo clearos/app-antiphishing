eba9960619ca69486d7287c27974e0377e94e653
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:56:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'ਗੇਟਵੇ Antiphishing ਐਪਲੀਕੇਸ਼ ਨੂੰ ਵੈੱਬ, FTP, ਮੇਲ ਅਤੇ ਹੋਰ ਨੂੰ ਸਕੈਨ ਕਰਨ ਲਈ ਇੱਕ ਮੱਧ antiphishing ਇੰਜਣ ਵਰਤਦਾ ਹੈ. ਇਹ ਇੰਜਣ ਦੁਆਰਾ ਖੋਜਿਆ ਖਤਰਨਾਕ ਲਿੰਕ ਤੱਕ ਉਪਭੋਗੀ ਨੂੰ ਬਚਾ ਕੇ ਆਪਣੇ ਨੈੱਟਵਰਕ ਨਾਲ ਜੁੜੇ ਜੰਤਰ ਦੀ ਰੱਖਿਆ ਕਰਦਾ ਹੈ.';
+$lang['antiphishing_app_name'] = 'ਗੇਟਵੇ Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'ਵੀ ਸਭ ਡਿਵੈਲਰਪਰ ਇੰਟਰਨੈੱਟ &#39;ਉਪਭੋਗੀ ਅਚਾਨਕ, ਇੱਕ ਫਿਸ਼ਿੰਗ ਲਿੰਕ&#39; ਤੇ ਕਲਿਕ ਕਰ ਸਕਦੇ ਹੋ. ਸਾਨੂੰ ਸਭ ਨੂੰ antiphishing ਫੀਚਰ ਨੂੰ ਯੋਗ ਨੂੰ ਛੱਡ ਕੇ ਸਿਫਾਰਸ਼.';
+$lang['antiphishing_block_cloaked'] = 'ਬਲਾਕ ਸਿੱਧ ਕੀਤੀ URL ਨੂੰ';
+$lang['antiphishing_block_ssl_mismatch'] = 'ਬਲਾਕ SSL ਨੂੰ ਮੇਲ';
+$lang['antiphishing_heuristics_engine'] = 'Heuristic ਇੰਜਣ';
+$lang['antiphishing_signature_engine'] = 'ਦਸਤਖਤ ਇੰਜਣ';
