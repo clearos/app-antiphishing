0f04593fab5db39bef844f6d323818064fe7a9c4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:52:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'App a Gateway antiphishing sèvi ak yon motè antiphishing santral nan analysis sou sit wèb ★ ftp, lapòs ak plis ankò. Li pwoteje aparèy konekte nan rezo ou pa pwoteksyon itilizatè yo soti nan lyen move detekte avèk motè a.';
+$lang['antiphishing_app_name'] = 'Gateway antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Menm itilizatè sa yo sou Entènèt pi konprandr ka aksidantèlman klike sou yon lyen èskrokri. Nou rekòmande kite tout karakteristik antiphishing pèmèt.';
+$lang['antiphishing_block_cloaked'] = 'Blòk cloaked URL';
+$lang['antiphishing_block_ssl_mismatch'] = 'Blòk SSL Matche';
+$lang['antiphishing_heuristics_engine'] = 'eristik Engine';
+$lang['antiphishing_signature_engine'] = 'siyati Engine';
