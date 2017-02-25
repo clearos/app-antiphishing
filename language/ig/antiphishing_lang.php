dbcdd5aa328d155dc90c793f30a23704e7ee9c35
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:53:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'The Gateway Antiphishing ngwa na-eji a Central antiphishing engine ka iṅomi web, FTP, mail na ndị ọzọ. Ọ na-echebe ngwaọrụ jikọọ na netwọk gị site echebe ndị ọrụ si obi njikọ ahụrụ site engine.';
+$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Ọbụna ndị kasị savvy Internet ọrụ nwere ike na mberede pịa a phishing njikọ. Anyị na-akwado ahapụ ihe niile antiphishing atụmatụ nyeere.';
+$lang['antiphishing_block_cloaked'] = 'Ngọngọ Cloaked URLs';
+$lang['antiphishing_block_ssl_mismatch'] = 'Ngọngọ SSL mismatch';
+$lang['antiphishing_heuristics_engine'] = 'Heuristic Engine';
+$lang['antiphishing_signature_engine'] = 'mbinye aka Engine';
