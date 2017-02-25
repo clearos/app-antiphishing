c918fae5b9b7c148637cb87439383f06d2e39445
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:52:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gl/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'A aplicación de pasarela Antiphishing usa un motor antiphishing central para dixitalizar web, FTP, correo electrónico e moito máis. El protexe os dispositivos conectados á súa rede, protexendo os usuarios de enlaces maliciosos detectados polo mecanismo.';
+$lang['antiphishing_app_name'] = 'pasarela Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Mesmo os usuarios de Internet máis expertos poden accidentalmente facer clic nun enlace de phishing. Recomendamos deixar todos os recursos antiphishing activado.';
+$lang['antiphishing_block_cloaked'] = 'URL bloque Cloaked';
+$lang['antiphishing_block_ssl_mismatch'] = 'Bloque SSL Incompatibilidade';
+$lang['antiphishing_heuristics_engine'] = 'motor heurístico';
+$lang['antiphishing_signature_engine'] = 'Motor de subscrición';
