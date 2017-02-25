c50c45239f0bccce72efbe78eeafd9f5c8a88b90
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:53:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'Úsáideann an app Gateway AntiPhishing inneall AntiPhishing lárnach a scanadh gréasáin, FTP, post agus níos mó. Cosnaíonn sé feistí ceangailte le do líonra ag úsáideoirí ó naisc mailíseach deara ag an innill sciath.';
+$lang['antiphishing_app_name'] = 'Geata AntiPhishing';
+$lang['antiphishing_app_tooltip'] = 'Is féidir fiú na n-úsáideoirí Idirlín is savvy cliceáil thaisme ar nasc phishing. Molaimid ag fágáil na gnéithe AntiPhishing cumasaithe.';
+$lang['antiphishing_block_cloaked'] = 'Bloc cloaked URLanna';
+$lang['antiphishing_block_ssl_mismatch'] = 'Bloc SSL Mismatch';
+$lang['antiphishing_heuristics_engine'] = 'Inneall heorastúla';
+$lang['antiphishing_signature_engine'] = 'Inneall síniú';
