54e571339010cf3436ff36d43d8afdd43963acd2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:51:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fr/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'L&#39;application passerelle Antiphishing utilise un moteur de antiphishing central pour scanner web, FTP, messagerie et plus encore. Il protège les périphériques connectés à votre réseau en protégeant les utilisateurs des liens malveillants détectés par le moteur.';
+$lang['antiphishing_app_name'] = 'Passerelle Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Même les utilisateurs d&#39;Internet les plus avertis peuvent accidentellement cliquer sur un lien de phishing. Nous recommandons de laisser toutes les fonctionnalités d&#39;antiphishing activées.';
+$lang['antiphishing_block_cloaked'] = 'Bloc Cloaked URL';
+$lang['antiphishing_block_ssl_mismatch'] = 'Bloquer SSL Mismatch';
+$lang['antiphishing_heuristics_engine'] = 'moteur heuristique';
+$lang['antiphishing_signature_engine'] = 'Signature Engine';
