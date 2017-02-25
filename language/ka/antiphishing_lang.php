72f6921964d27355508e0456fd3c5865fb445f2f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:54:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'კარიბჭე Antiphishing app იყენებს ცენტრალური AntiPhishing ძრავის სკანირების ვებ, FTP, ფოსტა და სხვა. ეს იცავს მოწყობილობების დაკავშირებული თქვენი ქსელის დამცავი წევრებს მუქარის ბმულები მიერ გამოვლენილი სისტემა.';
+$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'მაშინაც კი, ყველაზე საზრიანი ინტერნეტის მომხმარებლებს შეუძლიათ შემთხვევით დააწკაპუნეთ ფიშინგი ბმული. ჩვენ რეკომენდაციას გასვლის ყველა AntiPhishing მახასიათებლები ჩართულია.';
+$lang['antiphishing_block_cloaked'] = 'Block Cloaked მისამართები';
+$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL Mismatch';
+$lang['antiphishing_heuristics_engine'] = 'heuristic ძრავის';
+$lang['antiphishing_signature_engine'] = 'ხელმოწერა ძრავის';
