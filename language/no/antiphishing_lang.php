906f58a43c1a4b12b25189b18e0d664266f52c42
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:56:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/no/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'The Gateway Antiphishing app bruker en sentral antiphishing-motor for å skanne web, FTP, e-post og mer. Det beskytter enheter som er koblet til nettverket ved skjerming brukere fra ondsinnede lenker oppdages av motoren.';
+$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Selv de mest erfarne Internett-brukere ved et uhell kan klikke på en phishing link. Vi anbefaler å la alle antiphishing funksjoner aktivert.';
+$lang['antiphishing_block_cloaked'] = 'Block Maskert webadresser';
+$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL Mismatch';
+$lang['antiphishing_heuristics_engine'] = 'heuristisk Engine';
+$lang['antiphishing_signature_engine'] = 'signatur Engine';
