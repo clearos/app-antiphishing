393f7de8e942dda2f750e01b8f68f6bab9eed94f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:58:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sl/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'Gateway Antiphishing aplikacija uporablja osrednji motor ribarjenjem za skeniranje spletnega, FTP, pošto in več. Ščiti naprave, povezane z omrežjem, ki ga ščiti uporabnike pred zlonamernimi povezavami, ki jih motor odkritih.';
+$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Tudi najbolj zdrava pamet uporabnikov interneta se lahko po nesreči kliknite na phishing povezavo. Priporočamo, da vse funkcije pred ribarjenjem omogočene.';
+$lang['antiphishing_block_cloaked'] = 'Block Zakrita URL';
+$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL Neujemanje';
+$lang['antiphishing_heuristics_engine'] = 'hevristična motorja';
+$lang['antiphishing_signature_engine'] = 'podpis motorja';
