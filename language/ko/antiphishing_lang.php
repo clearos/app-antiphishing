60f6ce8d460165d350a60de8aa9ee8ed2e2f8aca
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:54:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = '게이트웨이 안티 피싱 앱은 웹, FTP, 메일 등을 스캔 중앙 안티 피싱 엔진을 사용합니다. 이 엔진에 의해 감지 된 악성 링크에서 사용자를 차단하여 네트워크에 연결된 장치를 보호합니다.';
+$lang['antiphishing_app_name'] = '게이트웨이 안티 피싱';
+$lang['antiphishing_app_tooltip'] = '심지어 가장 잘 아는 인터넷 사용자가 실수로 피싱 링크를 클릭 할 수 있습니다. 우리가 사용하는 모든 안티 피싱 기능을 남겨 두는 것이 좋습니다.';
+$lang['antiphishing_block_cloaked'] = '블록 은폐 된 URL';
+$lang['antiphishing_block_ssl_mismatch'] = '블록 SSL 불일치';
+$lang['antiphishing_heuristics_engine'] = '휴리스틱 엔진';
+$lang['antiphishing_signature_engine'] = '서명 엔진';
