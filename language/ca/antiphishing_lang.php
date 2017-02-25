431b98cc7a60de9b3a13d76814af5812aa3022ee
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:50:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'L&#39;aplicació de porta d&#39;enllaç Antiphishing utilitza un motor de pesca electrònica central per escanejar web, FTP, correu electrònic i més. Protegeix els dispositius connectats a la xarxa mitjançant el blindatge dels usuaris d&#39;enllaços maliciosos detectats pel motor.';
+$lang['antiphishing_app_name'] = 'Porta d&#39;enllaç Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Fins i tot els usuaris més coneixedors d&#39;Internet accidentalment poden fer clic en un enllaç de phishing. Es recomana deixar totes les característiques pesca electrònica habilitats.';
+$lang['antiphishing_block_cloaked'] = 'Bloc Cloaked URL';
+$lang['antiphishing_block_ssl_mismatch'] = 'Bloc SSL no coincident';
+$lang['antiphishing_heuristics_engine'] = 'motor heurístic';
+$lang['antiphishing_signature_engine'] = 'motor signatura';
