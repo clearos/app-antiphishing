d2004ab7577ea249132707b6407ff3b15c703963
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:58:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'The app Gateway Antiphishing isticmaalaa engine a antiphishing dhexe in iskaan web, FTP, mail oo dheeraad ah. Wuxuu ka hortagaa qalabka ku xiran aad network by gaashaamashada users ka links xaasidnimo ah la ogaado by engine ah.';
+$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Xataa dadka isticmaala Internet ugu bixio filaan ah riixi kartaa link phishing ah. Waxaan ku talineynaa tago oo dhan muuqaalada antiphishing karti.';
+$lang['antiphishing_block_cloaked'] = 'Block Cloaked URLs';
+$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL jaanqaadayn';
+$lang['antiphishing_heuristics_engine'] = 'Engine Heuristic';
+$lang['antiphishing_signature_engine'] = 'Saxiixa Engine';
