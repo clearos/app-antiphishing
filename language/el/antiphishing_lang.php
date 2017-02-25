cf5f7862bb5eae4861eae94c88916202af76497f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:52:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'Η εφαρμογή πύλη Antiphishing χρησιμοποιεί μια κεντρική μηχανή antiphishing να σαρώσετε web, FTP, ταχυδρομείο και πολλά άλλα. Προστατεύει τις συσκευές που συνδέονται στο δίκτυό σας με την θωράκιση των χρηστών από κακόβουλες συνδέσεις που ανιχνεύεται από τον κινητήρα.';
+$lang['antiphishing_app_name'] = 'πύλη Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Ακόμη και οι πιο συνειδητοποιημένοι χρήστες του Διαδικτύου μπορούν κατά λάθος κλικ σε έναν σύνδεσμο phishing. Συστήνουμε αφήνοντας όλα τα χαρακτηριστικά antiphishing ενεργοποιημένη.';
+$lang['antiphishing_block_cloaked'] = 'Αποκλεισμός Cloaked διευθύνσεις URL';
+$lang['antiphishing_block_ssl_mismatch'] = 'Αποκλεισμός SSL Ασυμφωνία';
+$lang['antiphishing_heuristics_engine'] = 'ευρετική μηχανή';
+$lang['antiphishing_signature_engine'] = 'μηχανή υπογραφή';
