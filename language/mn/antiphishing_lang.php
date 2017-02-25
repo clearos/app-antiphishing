032dabc14afe08bbca5e13b7c1c4cb558241cde3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:55:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'Gateway Antiphishing апп вэб, FTP, шуудан, илүү скан төв antiphishing хөдөлгүүрийг ашигладаг. Энэ хамгаалсан хөдөлгүүртэй илэрсэн хортой холбоосууд хэрэглэгчдийг таны сүлжээнд холбогдсон төхөөрөмжийг хамгаалдаг.';
+$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Тэр ч байтугай хамгийн ухаантай Интернэт хэрэглэгчид санамсаргүй нь фишинг холбоос дээр дарна уу. Бид боломжтой бүх antiphishing боломжуудыг орхиж зөвлөж байна.';
+$lang['antiphishing_block_cloaked'] = 'Блок Cloaked URL-ууд';
+$lang['antiphishing_block_ssl_mismatch'] = 'Блок SSL таарахгүй';
+$lang['antiphishing_heuristics_engine'] = 'Сэтгэн олох Engine';
+$lang['antiphishing_signature_engine'] = 'Гарын үсэг Engine';
