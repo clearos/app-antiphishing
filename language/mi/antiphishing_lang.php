73f7b0e6f36a2caf495d5117d8bad80ffe08b2e5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:55:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'Ka whakamahia e te taupānga Gateway Antiphishing te pūkaha Antiphishing pokapū ki te matawai i tukutuku, FTP, mēra me te ake. tiaki te reira i whakaaro hono ki tou whatunga mā te paruru kaiwhakamahi i hononga kino kitea e te pūkaha.';
+$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Ka taea e aitua ara nga kaiwhakamahi Ipurangi tino matika pāwhiri i runga i te hono hītinihanga. tūtohu tatou mahue āhuatanga Antiphishing katoa whakahohea.';
+$lang['antiphishing_block_cloaked'] = 'Poraka Rangatira URL';
+$lang['antiphishing_block_ssl_mismatch'] = 'Poraka SSL Koreōrite';
+$lang['antiphishing_heuristics_engine'] = 'Engine Heuristic';
+$lang['antiphishing_signature_engine'] = 'Engine waitohu';
