959d5fdd39c614a9216d4b99a87394ebb883e2ae
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:59:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'நுழைவாயில் Antiphishing பயன்பாட்டை வலை, FTP மின்னஞ்சல் மேலும் ஸ்கேன் ஒரு மத்திய antiphishing இயந்திரம் பயன்படுத்தும். அது இயந்திரம் மூலம் கண்டறியப்பட்டது தீங்கிழைக்கும் இணைப்புகள் இருந்து பயனர்கள் காப்பாக மூலம் உங்கள் நெட்வொர்க் இணைக்கப்பட்ட சாதனங்கள் பாதுகாக்கிறது.';
+$lang['antiphishing_app_name'] = 'நுழைவாயில் Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'மிகவும் ஆர்வலராகவும் இணைய பயனர்கள் கூட தற்செயலாக ஒரு ஃபிஷிங் இணைப்பை கிளிக் செய்யலாம். நாம் செயல்படுத்தப்படும் அனைத்து antiphishing அம்சங்கள் விட்டு பரிந்துரைக்கிறோம்.';
+$lang['antiphishing_block_cloaked'] = 'பிளாக் பொய்த்தோற்றப் URL கள்';
+$lang['antiphishing_block_ssl_mismatch'] = 'பிளாக், SSL மிஸ்மேட்ச்';
+$lang['antiphishing_heuristics_engine'] = 'ஹியூரெஸ்டிக் பொறி';
+$lang['antiphishing_signature_engine'] = 'கையொப்பம் பொறி';
