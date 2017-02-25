44b5e1c1f70afdc1dcde9b212dea17f2d78c70d4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:51:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = '网关反钓鱼应用程序使用一个中央反钓鱼引擎来扫描Web，FTP，邮件等等。 它保护通过屏蔽从引擎检测到的恶意链接的用户连接到您的网络设备。';
+$lang['antiphishing_app_name'] = '网关反钓鱼';
+$lang['antiphishing_app_tooltip'] = '即便是最精明的互联网用户可以小心点击钓鱼链接。 我们建议将所有启用的反钓鱼功能。';
+$lang['antiphishing_block_cloaked'] = '阻止隐形网址';
+$lang['antiphishing_block_ssl_mismatch'] = '块SSL不匹配';
+$lang['antiphishing_heuristics_engine'] = '启发式引擎';
+$lang['antiphishing_signature_engine'] = '签名引擎';
