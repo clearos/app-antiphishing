c6380e7f08a2cc2c327594370d526190a33f63c4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:51:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'La Enirejo Antiphishing app uzas centran antiphishing motoro skani retejo, FTP, retpoŝto kaj pli. Ĝi protektas mekanismoj konektitaj al via reto per ŝirmado uzantoj de malicaj ligoj detektitaj de la motoro.';
+$lang['antiphishing_app_name'] = 'enirejo Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Eĉ la plej sagacaj retumantoj povas hazarde alklaku phishing ligilon. Ni rekomendas lasi ĉiuj antiphishing karakterizaĵoj ebligita.';
+$lang['antiphishing_block_cloaked'] = 'Bloko cloaked URLoj';
+$lang['antiphishing_block_ssl_mismatch'] = 'Bloko SSL mismatch';
+$lang['antiphishing_heuristics_engine'] = 'heŭristiko Motoro';
+$lang['antiphishing_signature_engine'] = 'signature Motoro';
