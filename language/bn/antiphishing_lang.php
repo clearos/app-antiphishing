3b66d063c9fcbc6c9d86f3a01b1a366b186af9eb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:50:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'গেটওয়ে Antiphishing অ্যাপ্লিকেশন ওয়েব, এফটিপি, মেইল ​​এবং আরো স্ক্যান করার জন্য একটি কেন্দ্রীয় antiphishing ইঞ্জিন ব্যবহার করে. এটা ইঞ্জিন দ্বারা সনাক্ত দূষিত লিঙ্ক থেকে ব্যবহারকারীদের রক্ষা দ্বারা আপনার নেটওয়ার্কের সাথে সংযুক্ত ডিভাইস থেকে রক্ষা করে.';
+$lang['antiphishing_app_name'] = 'গেটওয়ে Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'এমনকি সবচেয়ে কাণ্ডজ্ঞান ইন্টারনেট ব্যবহারকারী ঘটনাক্রমে একটি ফিশিং লিঙ্কে ক্লিক করতে পারেন. আমরা সব antiphishing বৈশিষ্ট্য সক্রিয় ছাড়ার সুপারিশ করছি.';
+$lang['antiphishing_block_cloaked'] = 'ব্লক প্রতারণাপূর্ণ URL গুলি';
+$lang['antiphishing_block_ssl_mismatch'] = 'ব্লক এসএসএল মেলেনি';
+$lang['antiphishing_heuristics_engine'] = 'অনুসন্ধানমূলক ইঞ্জিন';
+$lang['antiphishing_signature_engine'] = 'স্বাক্ষর ইঞ্জিন';
