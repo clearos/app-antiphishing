3dc92ae0df38210ca3bf1dbbe84db30302c85385
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:57:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'ගේට්වේ Antiphishing යෙදුම වෙබ්, FTP, තැපැල් හා වඩා පරිලෝකනය කිරීමට මධ්යම antiphishing එන්ජිම භාවිතා කරයි. එය එන්ජින් මගින් අනාවරණය අනිෂ්ට සබැඳි වලින් පරිශීලකයන් ආරක්ෂා කරමින් ඔබගේ ජාල සම්බන්ධ උපකරණ ආරක්ෂා කරයි.';
+$lang['antiphishing_app_name'] = 'ගේට්වේ Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'වඩාත් රස විඳින්නන්, අන්තර්ජාල භාවිතා කරන්නන් පවා අහම්බෙන් හැගවීම සම්බන්ධතාවය මත ක්ලික් කරන්න පුළුවන්. අප සියලු antiphishing විශේෂාංග සක්රීය පිටත් නිර්දේශ කරමු.';
+$lang['antiphishing_block_cloaked'] = 'වාරණ ජියා URL ලිපින';
+$lang['antiphishing_block_ssl_mismatch'] = 'වාරණ SSL නොගැලපීම';
+$lang['antiphishing_heuristics_engine'] = 'කලපුවේ කිලෝමීටරයක් ​​එන්ජින්';
+$lang['antiphishing_signature_engine'] = 'අත්සන එන්ජින්';
