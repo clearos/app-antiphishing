0f9adfd8041d15096c4721a43ae6a36dbeb68846
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:59:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'Gateway nätfiske app använder en central nätfiske motor för att skanna webben, FTP, e-post och mer. Det skyddar enheter som är anslutna till nätverket genom avskärmning användare från skadliga länkar som upptäckts av motorn.';
+$lang['antiphishing_app_name'] = 'gateway nätfiske';
+$lang['antiphishing_app_tooltip'] = 'Även de mest kunniga Internetanvändare kan av misstag klicka på en nätfiskelänk. Vi rekommenderar att du lämnar alla nätfiske funktioner aktiverade.';
+$lang['antiphishing_block_cloaked'] = 'Block Cloaked adresser';
+$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL Mismatch';
+$lang['antiphishing_heuristics_engine'] = 'heuristisk motor';
+$lang['antiphishing_signature_engine'] = 'signatur Engine';
