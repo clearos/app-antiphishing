55d4ce6aed734c91b604181d25a457c171bb018b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:00:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'Gateway antiphishing App veb-, FTP, pochta va yana harakatlanish uchun markaziy antiphishing qidiruvi foydalanadi. Bu vosita tomonidan aniqlangan zararli ulanish foydalanuvchilarning himoya bilan tarmoqqa ulangan qurilmalar himoya qiladi.';
+$lang['antiphishing_app_name'] = 'Gateway antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Hatto, eng tushunadigan Internet foydalanuvchilari tasodifan bir fishing tugmasini bosing mumkin. Biz yoqilgan barcha antiphishing xususiyatlarini qoldirib tavsiya qilamiz.';
+$lang['antiphishing_block_cloaked'] = 'Blok yashirin URL';
+$lang['antiphishing_block_ssl_mismatch'] = 'Blok SSL turi mos kelmadi';
+$lang['antiphishing_heuristics_engine'] = 'evristik Engine';
+$lang['antiphishing_signature_engine'] = 'Imzo Engine';
