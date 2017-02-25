1d16162bf348c85d2b545b89d7774c3ec43f9957
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:55:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'Ny Gateway Antiphishing fampiharana mampiasa ny foibe antiphishing maotera ny scan aterineto, FTP, namany Sary sy ny maro hafa. Tsy miaro ny fitaovana mifandray amin&#39;ny tambajotra amin&#39;ny alalan&#39;ny miaro mpampiasa avy mampidi-doza ny rohy hita ny maotera.';
+$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Na dia ny tena savvy mpampiasa aterineto dia afaka nahy Tsindrio eo amin&#39;ny phishing rohy. Tsy fahazoan-dalana niala antiphishing rehetra endri-javatra alefa.';
+$lang['antiphishing_block_cloaked'] = 'Block Cloaked URLs';
+$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL Mismatch';
+$lang['antiphishing_heuristics_engine'] = 'Heuristic Engine';
+$lang['antiphishing_signature_engine'] = 'Signature Engine';
