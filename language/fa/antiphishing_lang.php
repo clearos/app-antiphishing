fa5437644c092d8b1834cab90314aef4e8be95a4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:57:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'برنامه دروازه آنتی فیشینگ استفاده از یک موتور آنتی فیشینگ مرکزی برای اسکن وب، FTP، ایمیل و بیشتر. این دستگاه توسط کاربران را از لینک های مخرب شناسایی شده توسط موتور به شبکه شما متصل محافظت می کند.';
+$lang['antiphishing_app_name'] = 'دروازه آنتی فیشینگ';
+$lang['antiphishing_app_tooltip'] = 'حتی کاربران اینترنت زرنگ و دانا ترین به طور تصادفی می توانید بر روی لینک های فیشینگ را کلیک کنید. ما توصیه می کنیم از ترک تمام ویژگی های آنتی فیشینگ را فعال کنید.';
+$lang['antiphishing_block_cloaked'] = 'بلوک پردهی آدرس ها';
+$lang['antiphishing_block_ssl_mismatch'] = 'بلوک SSL عدم تطابق';
+$lang['antiphishing_heuristics_engine'] = 'موتور اکتشافی';
+$lang['antiphishing_signature_engine'] = 'امضاء موتور';
