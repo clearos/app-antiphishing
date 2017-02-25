cad383b297b5b79340252975bd3f25544fc1cf5c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:49:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'App Gateway Mbrojtja Antiphishing përdor një motor qendror antiphishing të scan web, FTP, postën dhe më shumë. Ajo mbron pajisjet e lidhura me rrjetin tuaj duke mbrojtur përdoruesit nga lidhjet me qëllim të keq zbuluar nga motori.';
+$lang['antiphishing_app_name'] = 'Gateway Mbrojtja Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Edhe përdoruesit më të tru internetit mund të aksidentalisht klikoni mbi një lidhje phishing. Ne rekomandojmë duke i lënë të gjitha karakteristikat antiphishing aktivizuar.';
+$lang['antiphishing_block_cloaked'] = 'Blloku cloaked URL';
+$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL Mismatch';
+$lang['antiphishing_heuristics_engine'] = 'Engine deduktive';
+$lang['antiphishing_signature_engine'] = 'Engine Nënshkrimi';
