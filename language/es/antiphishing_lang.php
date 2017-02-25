677ffd5aa5b80c62833dc47f98b092f6561ed2ca
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:58:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/es/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'La aplicación de puerta de enlace Antiphishing utiliza un motor de antiphishing central para escanear Web, FTP, correo electrónico y más. Protege los dispositivos conectados a la red mediante el blindaje de los usuarios de enlaces maliciosos detectados por el motor.';
+$lang['antiphishing_app_name'] = 'Puerta de enlace Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Incluso los usuarios más conocedores de Internet accidentalmente pueden hacer clic en un enlace de phishing. Se recomienda dejar todas las características antiphishing habilitados.';
+$lang['antiphishing_block_cloaked'] = 'Bloque Cloaked URL';
+$lang['antiphishing_block_ssl_mismatch'] = 'Bloque SSL no coincidente';
+$lang['antiphishing_heuristics_engine'] = 'motor heurístico';
+$lang['antiphishing_signature_engine'] = 'motor firma';
