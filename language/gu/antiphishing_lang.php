a66438db4aee0f412f0c65af24a802f7fbc59998
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:52:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'ગેટવે Antiphishing એપ્લિકેશન વેબ, FTP, મેલ અને વધુ સ્કેન કરવા માટે કેન્દ્રીય antiphishing એન્જિન વાપરે છે. તે એન્જિન દ્વારા શોધાયેલ દૂષિત લિંક્સ વપરાશકર્તાઓ રક્ષણ કરીને તમારા નેટવર્ક સાથે જોડાયેલ ઉપકરણો રક્ષણ આપે છે.';
+$lang['antiphishing_app_name'] = 'ગેટવે Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'પણ મોટા ભાગના સમજશકિત ઈન્ટરનેટ વપરાશકર્તાઓ આકસ્મિક ફિશિંગ લિંક પર ક્લિક કરી શકો છો. અમે બધા antiphishing સુવિધાઓ સક્ષમ છોડીને ભલામણ કરીએ છીએ.';
+$lang['antiphishing_block_cloaked'] = 'બ્લોક અપ્રામાણિક URL ને';
+$lang['antiphishing_block_ssl_mismatch'] = 'બ્લોક SSL મિસ મેચ';
+$lang['antiphishing_heuristics_engine'] = 'સંશોધનાત્મક એન્જિન';
+$lang['antiphishing_signature_engine'] = 'હસ્તાક્ષર એન્જિન';
