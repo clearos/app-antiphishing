525855883eea2bc360a41898387ad257316d1e61
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:58:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'programu Gateway AntiPhishing anatumia kati AntiPhishing injini Scan mtandao, FTP, barua pepe na zaidi. Ni kulinda vifaa kushikamana na mtandao wako kwa kuzuia watumiaji kutoka viungo malicious wanaona na injini.';
+$lang['antiphishing_app_name'] = 'Gateway AntiPhishing';
+$lang['antiphishing_app_tooltip'] = 'Hata watumiaji wengi savvy Internet unaweza click ajali juu ya kiungo hadaa. Sisi kupendekeza kuacha makala yote AntiPhishing kuwezeshwa.';
+$lang['antiphishing_block_cloaked'] = 'Block cloaked URLs';
+$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL Mismatch';
+$lang['antiphishing_heuristics_engine'] = 'heuristic injini za';
+$lang['antiphishing_signature_engine'] = 'Signature injini za';
