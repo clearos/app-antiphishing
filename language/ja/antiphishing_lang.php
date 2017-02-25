edbd59a52c665c2a3aea1c2c3aaf44e009f9dfa4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:53:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'ゲートウェイアンチフィッシングアプリは、Web、FTP、メールなどをスキャンするために、中央フィッシング対策エンジンを使用しています。 これは、エンジンによって検出された悪質なリンクからユーザーを保護することによって、ネットワークに接続された機器を保護します。';
+$lang['antiphishing_app_name'] = 'ゲートウェイアンチフィッシング';
+$lang['antiphishing_app_tooltip'] = '最も精通したインターネットユーザーは誤ってフィッシングリンクをクリックすることができます。 私たちは有効になってすべてのフィッシング対策機能を残すことをお勧めします。';
+$lang['antiphishing_block_cloaked'] = 'ブロッククロークのURL';
+$lang['antiphishing_block_ssl_mismatch'] = 'ブロックSSLの不一致';
+$lang['antiphishing_heuristics_engine'] = 'ヒューリスティックエンジン';
+$lang['antiphishing_signature_engine'] = 'シグニチャエンジン';
