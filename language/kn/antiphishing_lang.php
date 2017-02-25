c5edc67dde05f2e0a106c6134ff1398be342bfd0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:54:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'ಗೇಟ್ವೇ Antiphishing ಅಪ್ಲಿಕೇಶನ್ ವೆಬ್, FTP ಯ, ಮೇಲ್ ಮತ್ತು ಹೆಚ್ಚು ಸ್ಕ್ಯಾನ್ ಕೇಂದ್ರ antiphishing ಎಂಜಿನ್ ಅನ್ನು ಬಳಸುತ್ತದೆ. ಇದು ಎಂಜಿನ್ ಪತ್ತೆ ದುರುದ್ದೇಶಿತ ಕೊಂಡಿಗಳು ಬಳಕೆದಾರರನ್ನು ರಕ್ಷಿಸುವ ಮೂಲಕ ನಿಮ್ಮ ಜಾಲಕ್ಕೆ ಸಂಪರ್ಕ ಸಾಧನಗಳು ರಕ್ಷಿಸುತ್ತದೆ.';
+$lang['antiphishing_app_name'] = 'ಗೇಟ್ವೇ Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'ಅತ್ಯಂತ ಜಾಣತನ ಇಂಟರ್ನೆಟ್ ಬಳಕೆದಾರರು ಆಕಸ್ಮಿಕವಾಗಿ ಒಂದು ಫಿಶಿಂಗ್ ಲಿಂಕ್ ಮೇಲೆ ಕ್ಲಿಕ್ ಮಾಡಿ. ನಾವು ಕುಕೀ ಎಲ್ಲಾ antiphishing ವೈಶಿಷ್ಟ್ಯಗಳನ್ನು ಬಿಟ್ಟು ಶಿಫಾರಸು.';
+$lang['antiphishing_block_cloaked'] = 'ಬ್ಲಾಕ್ ಮರೆಮಾಡಿದ URL ಗಳು';
+$lang['antiphishing_block_ssl_mismatch'] = 'ಬ್ಲಾಕ್ ಎಸ್ಎಸ್ಎಲ್ ಹೊಂದಾಣಿಕೆಯಾಗದಿರುವಿಕೆ';
+$lang['antiphishing_heuristics_engine'] = 'ಅನ್ವೇಷಣ ಎಂಜಿನ್';
+$lang['antiphishing_signature_engine'] = 'ಸಹಿ ಎಂಜಿನ್';
