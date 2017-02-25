67bc6e1485539a594f22ab37eec0bc18b955221e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:57:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pt/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'O aplicativo gateway Antiphishing usa um motor antiphishing central para digitalizar web, FTP, e-mail e muito mais. Ele protege os dispositivos conectados à sua rede, protegendo os usuários de links maliciosos detectados pelo mecanismo.';
+$lang['antiphishing_app_name'] = 'gateway Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Mesmo os usuários de Internet mais experientes podem acidentalmente clicar em um link de phishing. Recomendamos deixar todos os recursos antiphishing ativado.';
+$lang['antiphishing_block_cloaked'] = 'URLs bloco Cloaked';
+$lang['antiphishing_block_ssl_mismatch'] = 'Bloco SSL Incompatibilidade';
+$lang['antiphishing_heuristics_engine'] = 'motor heurístico';
+$lang['antiphishing_signature_engine'] = 'Motor de assinatura';
