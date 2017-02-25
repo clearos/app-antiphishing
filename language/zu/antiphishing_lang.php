f6a945fc4dfb918dcc3f07bcdb82021fc356dda9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:01:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'Insiza Gateway Antiphishing lusebenzisa central antiphishing injini ukuskena web, FTP, mail nokuningi. Kuvikela amadivayisi kunethiwekhi yakho nge ukuzivikela abasebenzisi kusuka links enonya Kutsho injini.';
+$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Ngisho savvy kakhulu abasebenzisi Internet kungaba ngephutha uchofoze isixhumanisi iHluzo. Sincoma kusale zonke izici antiphishing inikwe amandla.';
+$lang['antiphishing_block_cloaked'] = 'Block Cloaked URLs';
+$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL Ukungafani';
+$lang['antiphishing_heuristics_engine'] = 'Engine yokuzifundela';
+$lang['antiphishing_signature_engine'] = 'Isiginesha Engine';
