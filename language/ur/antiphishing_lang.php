ceb774332361d09bb4dcbfb2ed1f5264a74531c8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:00:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'گیٹ وے Antiphishing اے پی پی کی ویب، FTP، میل اور زیادہ اسکین کرنے کے ایک مرکزی antiphishing انجن کا استعمال کرتا. اس کے انجن کی طرف سے پتہ درنساوناپورن لنکس سے صارفین کو بچانے کی طرف سے آپ کے نیٹ ورک سے منسلک آلات کی حفاظت کرتا ہے.';
+$lang['antiphishing_app_name'] = 'گیٹ وے Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'یہاں تک کہ سب سے زیادہ پریمی انٹرنیٹ صارفین اتفاقی طور پر ایک فریب دہی کے لنک پر کلک کر سکتے ہیں. ہم فعال تمام antiphishing خصوصیات کو چھوڑ سفارش کرتے ہیں.';
+$lang['antiphishing_block_cloaked'] = 'بلاک Al-Muddaththir شروع کے URLs';
+$lang['antiphishing_block_ssl_mismatch'] = 'بلاک SSL غیرمشابہ';
+$lang['antiphishing_heuristics_engine'] = 'انکشافی انجن';
+$lang['antiphishing_signature_engine'] = 'دستخط انجن';
