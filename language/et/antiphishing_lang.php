0b6add22254d99b10abf1b9de325b4fb97573ee7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:51:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'The Gateway antipettuse app kasutab keskne antipettuse mootori skaneerida web, FTP, posti ja rohkem. See kaitseb seadmeid võrku ühendatud varje kasutajate pahatahtliku lingid avastatud mootor.';
+$lang['antiphishing_app_name'] = 'Gateway antipettuse';
+$lang['antiphishing_app_tooltip'] = 'Isegi kõige Savvy Internet kasutajad saavad kogemata klõpsa phishing link. Soovitame jätta kõik antipettuse funktsioone lubatud.';
+$lang['antiphishing_block_cloaked'] = 'Block Kaetud URL';
+$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL mismatch';
+$lang['antiphishing_heuristics_engine'] = 'heuristiline Mootor';
+$lang['antiphishing_signature_engine'] = 'allkiri Mootor';
