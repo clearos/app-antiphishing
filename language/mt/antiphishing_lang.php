ae476482e4152a15b45cf9cd01d37123fc8b58a2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:56:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'Il-app Bieb antiphishing juża magna antiphishing ċentrali jiġu skennjati web, FTP, posta u aktar. Jipproteġi mezzi konnessi man-network tiegħek billi jħarisha utenti minn rabtiet malizzjużi skoperti mill-magna.';
+$lang['antiphishing_app_name'] = 'Gateway antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Anki l-utenti l-Internet aktar sofistikati jistgħu aċċidentalment ikklikkja fuq rabta phishing. Nirrakkomandaw li jħallu l-karatteristiċi kollha antiphishing ppermettiet.';
+$lang['antiphishing_block_cloaked'] = 'Blokk cloaked URLs';
+$lang['antiphishing_block_ssl_mismatch'] = 'Blokk SSL diskrepanza';
+$lang['antiphishing_heuristics_engine'] = 'magna heuristic';
+$lang['antiphishing_signature_engine'] = 'firma tal-magna';
