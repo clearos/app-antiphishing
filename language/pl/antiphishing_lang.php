7aac017959792b77592ff22d35b4c4a6b34e9055
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:57:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pl/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'Aplikacja Brama Antyphishing wykorzystuje centralny silnik antyphishingową skanować WWW, FTP, poczta i inne. Chroni urządzenie podłączone do sieci przez ekranowanie użytkowników przed złośliwymi linkami wykrytych przez silnik.';
+$lang['antiphishing_app_name'] = 'bramka Antyphishing';
+$lang['antiphishing_app_tooltip'] = 'Nawet najbardziej doświadczeni internauci mogą przypadkowo kliknąć na link phishing. Zalecamy pozostawienie wszystkich funkcji Antyphishing włączona.';
+$lang['antiphishing_block_cloaked'] = 'Blokowanie URL maskowana';
+$lang['antiphishing_block_ssl_mismatch'] = 'Blok SSL Niedopasowanie';
+$lang['antiphishing_heuristics_engine'] = 'heurystyczna';
+$lang['antiphishing_signature_engine'] = 'podpis Silnik';
