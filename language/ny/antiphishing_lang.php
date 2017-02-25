470ebd99f3d5a9adc85aa14cf9df1cc1ae2b11fd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:50:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'The app Polakwika Antiphishing amagwiritsa chapakati antiphishing injini kuti aone ukonde, FTP, makalata ndi zambiri. Kumatetezera zipangizo olumikiza maukonde anu ndi kutetezedwa owerenga ku limasonyeza njiru wapezeka ndi injini.';
+$lang['antiphishing_app_name'] = 'pachipata Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Ngakhale owerenga ambiri savvy Intaneti alemba mwangozi pa kugwirizana njira yofuna. Mpofunika kusiya zinthu zonse antiphishing chinathandiza.';
+$lang['antiphishing_block_cloaked'] = 'Block Cloaked maulalo';
+$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL Mismatch';
+$lang['antiphishing_heuristics_engine'] = 'Heuristic Engine';
+$lang['antiphishing_signature_engine'] = 'siginecha Engine';
