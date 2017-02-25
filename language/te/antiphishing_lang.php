e1d2425b30f00fffec55ed40b7690cfa22fdc917
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:59:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'గేట్వే Antiphishing అనువర్తనం వెబ్, FTP, మెయిల్ మరియు మరిన్ని స్కాన్ ఒక కేంద్ర antiphishing ఇంజిన్ను ఉపయోగిస్తుంది. ఇది ఇంజన్ ద్వారా కనుగొనబడింది హానికరమైన లింకులు నుండి వినియోగదారులు రక్షణ కల్పించడం ద్వారా మీ నెట్వర్క్కు కనెక్ట్ పరికరాలు రక్షిస్తుంది.';
+$lang['antiphishing_app_name'] = 'గేట్వే Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'కూడా చాలా అవగాహన ఇంటర్నెట్ వినియోగదారులు అనుకోకుండా ఒక ఫిషింగ్ లింక్పై క్లిక్ చెయ్యండి. మేము ఎనేబుల్ అన్ని antiphishing లక్షణాలు వదిలి సిఫార్సు.';
+$lang['antiphishing_block_cloaked'] = 'బ్లాక్ మోసపూరిత URL లు';
+$lang['antiphishing_block_ssl_mismatch'] = 'బ్లాక్ SSL అసమతుల్యత';
+$lang['antiphishing_heuristics_engine'] = 'సమస్యా పరిష్కారం ఇంజిన్';
+$lang['antiphishing_signature_engine'] = 'సంతకం ఇంజిన్';
