ad1c9fc8563babe041b1dd5dc64c8fa004fefe14
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:55:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'апликација Портал Antiphishing користи централна antiphishing моторот за скенирање на веб, FTP, пошта и повеќе. Таа го заштитува уреди поврзани на вашата мрежа со заштитен корисниците од малициозни линкови откриени од страна на моторот.';
+$lang['antiphishing_app_name'] = 'портал Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Дури и повеќето такт интернет корисници случајно може да кликнете на линкот за phishing. Ние препорачуваме оставајќи ги сите antiphishing овозможено можности.';
+$lang['antiphishing_block_cloaked'] = 'Забрани пелерина адреси';
+$lang['antiphishing_block_ssl_mismatch'] = 'Забрани SSL Несогласување';
+$lang['antiphishing_heuristics_engine'] = 'хеуристична';
+$lang['antiphishing_signature_engine'] = 'потпис на мотор';
