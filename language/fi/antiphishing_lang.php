87112a45dec5a479c99de0dc949f09f5ca6a220b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:51:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'Gateway Suojaus tietojen kalastelulta app käyttää keskeinen antiphishing moottori skannata web, FTP, sähköposti ja paljon muuta. Se suojaa laitteita kytketty verkkoon suojaamalla käyttäjiä haitallisilta linkit havaitsee moottorin.';
+$lang['antiphishing_app_name'] = 'Gateway Suojaus tietojen kalastelulta';
+$lang['antiphishing_app_tooltip'] = 'Jopa kaikkein taju Internetin käyttäjät voivat vahingossa napsauttaa phishing linkkiä. Suosittelemme jättäen kaikki antiphishing ominaisuudet ovat käytössä.';
+$lang['antiphishing_block_cloaked'] = 'Block Peitetty URL-osoitteita';
+$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL täsmää';
+$lang['antiphishing_heuristics_engine'] = 'Heuristic Engine';
+$lang['antiphishing_signature_engine'] = 'Allekirjoitus Engine';
