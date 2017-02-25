a86f07e2678fa298d7312544800351e9ad8a955e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:59:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/antiphishing_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['antiphishing_app_description'] = 'Ağ Geçidi Antiphishing uygulaması, web, FTP, mail ve daha taramak için merkezi bir antiphishing motoru kullanır. Bu motor tarafından tespit edilen kötü niyetli linkler kullanıcıları koruma ile ağınıza bağlı cihazların korur.';
+$lang['antiphishing_app_name'] = 'Ağ geçidi Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Hatta en deneyimli internet kullanıcılarının yanlışlıkla bir phishing bağlantısını tıklayabilirsiniz. Biz etkinleştirilmiş tüm antiphishing özellikleri bırakmanızı öneririz.';
+$lang['antiphishing_block_cloaked'] = 'Blok Gizlenmiş URL&#39;ler';
+$lang['antiphishing_block_ssl_mismatch'] = 'Blok SSL Uyuşmazlığı';
+$lang['antiphishing_heuristics_engine'] = 'Sezgisel Motor';
+$lang['antiphishing_signature_engine'] = 'İmza Motor';
