8e8f3daba8f2b14d4912770ac4af9f6b1fc8c69e
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-12 23:55:11 +0000

    Add new files and directories, commit changes, push changes

--- language/bn/antiphishing_lang.php
+++ language/bn/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'গেটওয়ে Antiphishing অ্যাপ্লিকেশন ওয়েব, এফটিপি, মেইল ​​এবং আরো স্ক্যান করার জন্য একটি কেন্দ্রীয় antiphishing ইঞ্জিন ব্যবহার করে. এটা ইঞ্জিন দ্বারা সনাক্ত দূষিত লিঙ্ক থেকে ব্যবহারকারীদের রক্ষা দ্বারা আপনার নেটওয়ার্কের সাথে সংযুক্ত ডিভাইস থেকে রক্ষা করে.';
+$lang['antiphishing_app_description'] = 'গেটওয়ে Antiphishing অ্যাপ্লিকেশনটি ওয়েব, এফটিপি, মেল এবং আরও স্ক্যান করার জন্য একটি কেন্দ্রীয় অ্যান্টিফিশিং ইঞ্জিন ব্যবহার করে। এটি ইঞ্জিন দ্বারা সনাক্ত দূষিত লিঙ্ক থেকে ব্যবহারকারীদের রক্ষা করে আপনার নেটওয়ার্কের সাথে সংযুক্ত ডিভাইসগুলি রক্ষা করে।';
 $lang['antiphishing_app_name'] = 'গেটওয়ে Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'এমনকি সবচেয়ে কাণ্ডজ্ঞান ইন্টারনেট ব্যবহারকারী ঘটনাক্রমে একটি ফিশিং লিঙ্কে ক্লিক করতে পারেন. আমরা সব antiphishing বৈশিষ্ট্য সক্রিয় ছাড়ার সুপারিশ করছি.';
-$lang['antiphishing_block_cloaked'] = 'ব্লক প্রতারণাপূর্ণ URL গুলি';
-$lang['antiphishing_block_ssl_mismatch'] = 'ব্লক এসএসএল মেলেনি';
-$lang['antiphishing_heuristics_engine'] = 'অনুসন্ধানমূলক ইঞ্জিন';
+$lang['antiphishing_app_tooltip'] = 'এমনকি সর্বাধিক সচেতন ইন্টারনেট ব্যবহারকারীরা ভুলভাবে একটি ফিশিং লিঙ্কটি ক্লিক করতে পারেন। আমরা সব antiphishing বৈশিষ্ট্যগুলি সক্ষমিত রাখার সুপারিশ করছি।';
+$lang['antiphishing_block_cloaked'] = 'ব্লক cloaked URL গুলি';
+$lang['antiphishing_block_ssl_mismatch'] = 'ব্লক SSL মিল';
+$lang['antiphishing_heuristics_engine'] = 'হিউরিস্টিক ইঞ্জিন';
 $lang['antiphishing_signature_engine'] = 'স্বাক্ষর ইঞ্জিন';
