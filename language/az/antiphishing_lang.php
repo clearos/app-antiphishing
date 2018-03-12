832e1ca872fb5d9225d784342ed7789205a5a962
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-12 23:53:38 +0000

    Add new files and directories, commit changes, push changes

--- language/az/antiphishing_lang.php
+++ language/az/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'Gateway Antifişinq app web, FTP, poçt və daha çox scan mərkəzi antiphishing engine istifadə edir. Bu mühərrik aşkar zərərli links istifadəçilər kalkan ilə şəbəkə bağlı cihazlar qoruyur.';
-$lang['antiphishing_app_name'] = 'Gateway Antifişinq';
-$lang['antiphishing_app_tooltip'] = 'Hətta ən fərasət Internet users təsadüfən bir phishing linki vurun. Biz imkan bütün antiphishing xüsusiyyətləri tərk gəlir.';
-$lang['antiphishing_block_cloaked'] = 'Block Cloaked URL&#39;leri';
-$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL uyğunsuzluq';
+$lang['antiphishing_app_description'] = 'Gateway Antiphishing proqramı veb, FTP, poçt və daha çox tarama üçün mərkəzi antiphishing mühərrikindən istifadə edir. Bu, istifadəçiləri mühərrik tərəfindən aşkar edilmiş zərərli əlaqələrdən qoruyan şəbəkənizə qoşulan cihazları qoruyur.';
+$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Ən həssas İnternet istifadəçiləri belə bir təsadüfən phishing linkinə basa bilərlər. Bütün antiphishing xüsusiyyətlərini aktivləşdirməyi tövsiyə edirik.';
+$lang['antiphishing_block_cloaked'] = 'Cloaked URL&#39;leri Engelle';
+$lang['antiphishing_block_ssl_mismatch'] = 'SSL Uyuşmazlığı Engelle';
 $lang['antiphishing_heuristics_engine'] = 'Heuristic Engine';
-$lang['antiphishing_signature_engine'] = 'Signature Engine';
+$lang['antiphishing_signature_engine'] = 'İmza mühərriki';
