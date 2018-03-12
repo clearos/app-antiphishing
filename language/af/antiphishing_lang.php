79b63d038854921daa1ecb2d06bb78cae38ab70c
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-12 23:51:36 +0000

    Add new files and directories, commit changes, push changes

--- language/af/antiphishing_lang.php
+++ language/af/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'Die Gateway Antiphishing app gebruik &#39;n sentrale AntiPhishing enjin web, FTP, pos en nog baie meer te scan. Dit beskerm toestelle gekoppel aan jou netwerk deur afskerming gebruikers van kwaadwillige skakels opgespoor word deur die enjin.';
+$lang['antiphishing_app_description'] = 'Die Gateway Antiphishing app gebruik &#39;n sentrale antiphishing-enjin om web, FTP, pos en meer te skandeer. Dit beskerm toestelle wat aan jou netwerk gekoppel is deur gebruikers te beskerm teen skadelike skakels wat deur die enjin opgespoor word.';
 $lang['antiphishing_app_name'] = 'Gateway Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'Selfs die mees vaardig Internet gebruikers kan per ongeluk klik op &#39;n phishing skakel. Ons beveel verlaat al AntiPhishing funksies aangeskakel.';
-$lang['antiphishing_block_cloaked'] = 'Blok Gehul URLs';
-$lang['antiphishing_block_ssl_mismatch'] = 'Blok SSL Wanaanpassing';
-$lang['antiphishing_heuristics_engine'] = 'heuristiese enjin';
-$lang['antiphishing_signature_engine'] = 'handtekening Engine';
+$lang['antiphishing_app_tooltip'] = 'Selfs die mees vaardige internetgebruikers kan per ongeluk op &#39;n phishing-skakel klik. Ons raai u aan dat alle antifishing funksies geaktiveer word.';
+$lang['antiphishing_block_cloaked'] = 'Blok geslote URL&#39;s';
+$lang['antiphishing_block_ssl_mismatch'] = 'Blok SSL-afwyking';
+$lang['antiphishing_heuristics_engine'] = 'Heuristiese Motor';
+$lang['antiphishing_signature_engine'] = 'Handtekening Engine';
