545b8027ddf12304d2a9ad6a10748727b3cd74cf
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-12 23:57:34 +0000

    Add new files and directories, commit changes, push changes

--- language/ny/antiphishing_lang.php
+++ language/ny/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'The app Polakwika Antiphishing amagwiritsa chapakati antiphishing injini kuti aone ukonde, FTP, makalata ndi zambiri. Kumatetezera zipangizo olumikiza maukonde anu ndi kutetezedwa owerenga ku limasonyeza njiru wapezeka ndi injini.';
-$lang['antiphishing_app_name'] = 'pachipata Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'Ngakhale owerenga ambiri savvy Intaneti alemba mwangozi pa kugwirizana njira yofuna. Mpofunika kusiya zinthu zonse antiphishing chinathandiza.';
-$lang['antiphishing_block_cloaked'] = 'Block Cloaked maulalo';
-$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL Mismatch';
+$lang['antiphishing_app_description'] = 'Pulogalamu ya Antiway yamawindo imagwiritsira ntchito injini yapakati yoteteza antiphishing kuyesa webusaiti, FTP, mail ndi zina. Zimateteza mafoni ogwirizana ndi makanema anu pogwiritsa ntchito osuta kuchokera kuzilumikiza zoipa zomwe zimapezeka ndi injini.';
+$lang['antiphishing_app_name'] = 'Chipatala Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Ngakhale ogwiritsa ntchito kwambiri pa intaneti angawononge mwangozi chiyanjano chachinyengo. Tikukulimbikitsani kusiya zonse zothana ndi antiphishing zothandizidwa.';
+$lang['antiphishing_block_cloaked'] = 'Lembani URL Zobisika';
+$lang['antiphishing_block_ssl_mismatch'] = 'Dulani SSL Mismatch';
 $lang['antiphishing_heuristics_engine'] = 'Heuristic Engine';
-$lang['antiphishing_signature_engine'] = 'siginecha Engine';
+$lang['antiphishing_signature_engine'] = 'Engine Enginein';
