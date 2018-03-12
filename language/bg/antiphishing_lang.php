656c455a5cdc1c2f68cbc68bf4c13432c6a81e6d
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-12 23:56:00 +0000

    Add new files and directories, commit changes, push changes

--- language/bg/antiphishing_lang.php
+++ language/bg/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'Приложението за Gateway антифишинг използва централна антифишинг двигател за сканиране на уеб, FTP, поща и др. Той предпазва устройства, свързани към мрежата чрез екраниране потребители от злонамерени връзки, открити от двигателя.';
-$lang['antiphishing_app_name'] = 'Gateway антифишинг';
-$lang['antiphishing_app_tooltip'] = 'Дори и най-разбирам Интернет потребителите могат случайно да кликнат върху линк фишинг. Ние препоръчваме да остави всички антифишинг характеристики активирани.';
-$lang['antiphishing_block_cloaked'] = 'Блок забулен URL адреси';
-$lang['antiphishing_block_ssl_mismatch'] = 'Блок SSL Разминаване';
-$lang['antiphishing_heuristics_engine'] = 'евристични Engine';
-$lang['antiphishing_signature_engine'] = 'Подпис на двигателя';
+$lang['antiphishing_app_description'] = 'Приложението Gatephing Antiphishing използва централен антифишинг двигател за сканиране на уеб, FTP, поща и др. Той защитава устройствата, свързани с вашата мрежа, като предпазва потребителите от злонамерени връзки, открити от двигателя.';
+$lang['antiphishing_app_name'] = 'Gatephishing Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Дори най-разумните потребители на интернет могат случайно да кликнат върху фишинг връзка. Препоръчваме да оставите всички активирани антифишинг функции.';
+$lang['antiphishing_block_cloaked'] = 'Блокиране на прикритите URL адреси';
+$lang['antiphishing_block_ssl_mismatch'] = 'Блокиране на SSL несъответствие';
+$lang['antiphishing_heuristics_engine'] = 'Евристичен двигател';
+$lang['antiphishing_signature_engine'] = 'Подписващ двигател';
