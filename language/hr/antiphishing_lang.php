115a256e05769c9e604b78c7eccd21812b09343a
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-12 23:58:41 +0000

    Add new files and directories, commit changes, push changes

--- language/hr/antiphishing_lang.php
+++ language/hr/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'Gateway Antiphishing app koristi centralni antiphishing motor za skeniranje web, FTP, e-mail i još mnogo toga. Štiti uređaje povezane na mrežu oklapanje korisnike od zlonamjernih linkova otkrivene motora.';
+$lang['antiphishing_app_description'] = 'Aplikacija Gateway Antiphishing koristi središnji antiphishing motor za skeniranje weba, FTP-a, pošte i još mnogo toga. Ona štiti uređaje povezane s vašom mrežom tako da zaštiti korisnike od zlonamjernih veza koje je otkrio motor.';
 $lang['antiphishing_app_name'] = 'Gateway Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'Čak i većina pamet korisnici Interneta mogu slučajno kliknite na link phishing. Preporučujemo da sve značajke soft Antiphishing.';
-$lang['antiphishing_block_cloaked'] = 'Blok Maskirana URL-ova';
-$lang['antiphishing_block_ssl_mismatch'] = 'Blok SSL podudaraju';
-$lang['antiphishing_heuristics_engine'] = 'heuristički motora';
+$lang['antiphishing_app_tooltip'] = 'Čak i najiskusniji korisnici Interneta mogu slučajno kliknuti na vezu za krađu identiteta. Preporučujemo da omogućite sve značajke protiv filtriranja.';
+$lang['antiphishing_block_cloaked'] = 'Blokiraj Cloaked URL-ove';
+$lang['antiphishing_block_ssl_mismatch'] = 'Blokiraj SSL neusklađivanje';
+$lang['antiphishing_heuristics_engine'] = 'Heuristički motor';
 $lang['antiphishing_signature_engine'] = 'Potpis motora';
