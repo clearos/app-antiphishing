30b36c2b6954f3993d90cff4ccbbccbd2c82927c
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-12 23:56:45 +0000

    Add new files and directories, commit changes, push changes

--- language/my/antiphishing_lang.php
+++ language/my/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'အဆိုပါ Gateway မှာ Antiphishing app ကိုဝက်ဘ်, FTP, mail နဲ့ပိုပြီး scan တစ်ခုအလယ်ပိုင်း antiphishing အင်ဂျင်ကိုအသုံးပြုပါတယ်။ ဒါဟာအင်ဂျင်အားဖြင့်ရှာဖွေတွေ့ရှိအန္တရာယ်ရှိတဲ့လင့်များမှအသုံးပြုသူများကိုကွယ်ခြင်းအားဖြင့်သင့် network ကိုချိတ်ဆက် devices များကိုကာကွယ်ပေးသည်။';
+$lang['antiphishing_app_description'] = 'အဆိုပါ Gateway မှာ Antiphishing app ကိုဝက်ဘ်, FTP, mail နဲ့ပိုပြီး scan တစ်ခုအလယ်ပိုင်း antiphishing အင်ဂျင်ကိုအသုံးပြုပါတယ်။ ဒါဟာအင်ဂျင်အားဖြင့်ရှာဖွေတွေ့ရှိအန္တရာယ်ရှိတဲ့လင့်များကနေအသုံးပြုသူများကိုကာကှယျဖို့အားဖြင့်သင့်ကွန်ယက်ချိတ်ဆက်ပစ္စည်းတွေကိုကာကွယ်ပေးသည်။';
 $lang['antiphishing_app_name'] = 'gateway မှာ Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'တောင်မှအများဆုံးပါးရည်နပ်ရည်အင်တာနက်အသုံးပြုသူမတော်တဆတစ် phishing link ကို click နှိပ်ပြီးနိုင်ပါတယ်။ ကျနော်တို့ enabled အားလုံး antiphishing features တွေကိုစွန့်ခွာဖို့အကြံပြုလိုပါတယ်။';
-$lang['antiphishing_block_cloaked'] = 'block URLs များကိုရင်ခွင်';
-$lang['antiphishing_block_ssl_mismatch'] = 'block SSL ကမတိုက်ဆိုင်';
+$lang['antiphishing_app_tooltip'] = 'တောင်မှအများဆုံးပါးရည်နပ်ရည်အင်တာနက်အသုံးပြုသူမတော်တဆတစ် phishing link ကို click နှိပ်ပြီးနိုင်ပါတယ်။ ကျနော်တို့ enabled အားလုံး antiphishing features တွေထွက်ခွာအကြံပြုပါသည်။';
+$lang['antiphishing_block_cloaked'] = 'block ရင်ခွင် URLs များကို';
+$lang['antiphishing_block_ssl_mismatch'] = 'block SSL ကိုမတိုက်ဆိုင်';
 $lang['antiphishing_heuristics_engine'] = 'Heuristic အင်ဂျင်';
 $lang['antiphishing_signature_engine'] = 'လက်မှတအင်ဂျင်';
