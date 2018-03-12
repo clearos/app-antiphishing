5ad2a8ea8f472260d69ef29ab98d1a638cdbca3c
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-12 23:58:16 +0000

    Add new files and directories, commit changes, push changes

--- language/zh/antiphishing_lang.php
+++ language/zh/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = '网关反钓鱼应用程序使用一个中央反钓鱼引擎来扫描Web，FTP，邮件等等。 它保护通过屏蔽从引擎检测到的恶意链接的用户连接到您的网络设备。';
+$lang['antiphishing_app_description'] = 'Gateway Antiphishing应用使用中央反钓鱼引擎来扫描网页，FTP，邮件等。 它通过屏蔽用户免受引擎检测到的恶意链接来保护连接到网络的设备。';
 $lang['antiphishing_app_name'] = '网关反钓鱼';
-$lang['antiphishing_app_tooltip'] = '即便是最精明的互联网用户可以小心点击钓鱼链接。 我们建议将所有启用的反钓鱼功能。';
-$lang['antiphishing_block_cloaked'] = '阻止隐形网址';
-$lang['antiphishing_block_ssl_mismatch'] = '块SSL不匹配';
+$lang['antiphishing_app_tooltip'] = '即使是最精明的互联网用户也可能意外地点击钓鱼链接。 我们建议您启用所有反钓鱼功能。';
+$lang['antiphishing_block_cloaked'] = '阻止隐藏的网址';
+$lang['antiphishing_block_ssl_mismatch'] = '阻止SSL不匹配';
 $lang['antiphishing_heuristics_engine'] = '启发式引擎';
 $lang['antiphishing_signature_engine'] = '签名引擎';
