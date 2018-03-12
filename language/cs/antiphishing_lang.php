7c002d445bc5ed891bc7065725139b6021b8e619
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-12 23:59:06 +0000

    Add new files and directories, commit changes, push changes

--- language/cs/antiphishing_lang.php
+++ language/cs/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'Brána Antiphishing aplikace používá centrální phishingem engine pro skenování web, FTP, pošty a další. Chrání zařízení připojená k síti tím, že chrání uživatele před škodlivými odkazy zjištěných motoru.';
-$lang['antiphishing_app_name'] = 'brána Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'Dokonce i ty zdatní uživatelé internetu mohou náhodně kliknout na odkaz phishing. Doporučujeme ponechat všechny funkce AntiPhishing povolena.';
-$lang['antiphishing_block_cloaked'] = 'Blokové Maskovaná URL';
-$lang['antiphishing_block_ssl_mismatch'] = 'Blok SSL Neshoda';
-$lang['antiphishing_heuristics_engine'] = 'heuristická Engine';
-$lang['antiphishing_signature_engine'] = 'podpis Engine';
+$lang['antiphishing_app_description'] = 'Aplikace Gateph Antiphishing používá pro kontrolu skenování webu, FTP, pošty a dalších. Chrání zařízení připojená k síti tím, že chrání uživatele před škodlivými odkazy zjištěnými motorem.';
+$lang['antiphishing_app_name'] = 'Gatephile Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Dokonce i nejvíce důvtipní uživatelé internetu mohou nechtěně kliknout na odkaz phishing. Doporučujeme ponechat všechny povolené funkce antiphishing.';
+$lang['antiphishing_block_cloaked'] = 'Blokovat adresy URL';
+$lang['antiphishing_block_ssl_mismatch'] = 'Zablokování SSL nesouhlasu';
+$lang['antiphishing_heuristics_engine'] = 'Heuristický motor';
+$lang['antiphishing_signature_engine'] = 'Podpisový motor';
