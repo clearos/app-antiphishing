edbf6c0d5d95b90a0b783c8639be8df724a86ff0
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:05:14 +0000

    Add new files and directories, commit changes, push changes

--- language/gu/antiphishing_lang.php
+++ language/gu/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'ગેટવે Antiphishing એપ્લિકેશન વેબ, FTP, મેલ અને વધુ સ્કેન કરવા માટે કેન્દ્રીય antiphishing એન્જિન વાપરે છે. તે એન્જિન દ્વારા શોધાયેલ દૂષિત લિંક્સ વપરાશકર્તાઓ રક્ષણ કરીને તમારા નેટવર્ક સાથે જોડાયેલ ઉપકરણો રક્ષણ આપે છે.';
-$lang['antiphishing_app_name'] = 'ગેટવે Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'પણ મોટા ભાગના સમજશકિત ઈન્ટરનેટ વપરાશકર્તાઓ આકસ્મિક ફિશિંગ લિંક પર ક્લિક કરી શકો છો. અમે બધા antiphishing સુવિધાઓ સક્ષમ છોડીને ભલામણ કરીએ છીએ.';
-$lang['antiphishing_block_cloaked'] = 'બ્લોક અપ્રામાણિક URL ને';
-$lang['antiphishing_block_ssl_mismatch'] = 'બ્લોક SSL મિસ મેચ';
-$lang['antiphishing_heuristics_engine'] = 'સંશોધનાત્મક એન્જિન';
-$lang['antiphishing_signature_engine'] = 'હસ્તાક્ષર એન્જિન';
+$lang['antiphishing_app_description'] = 'ગેટવે એન્ટિફિશિંગ એપ્લિકેશન વેબ, FTP, મેઇલ અને વધુને સ્કેન કરવા માટે કેન્દ્રિય એન્ટિફિશિંગ એન્જિનનો ઉપયોગ કરે છે. તે એન્જિન દ્વારા શોધાયેલ દૂષિત લિંક્સથી વપરાશકર્તાઓને સુરક્ષિત કરીને તમારા નેટવર્કથી કનેક્ટેડ ઉપકરણોનું રક્ષણ કરે છે.';
+$lang['antiphishing_app_name'] = 'ગેટવે એન્ટિફિશિંગ';
+$lang['antiphishing_app_tooltip'] = 'સૌથી વધુ જાણકાર ઈન્ટરનેટ યુઝર્સ પણ આકસ્મિક ફિશીંગ લિંક પર ક્લિક કરી શકે છે. અમે સક્ષમ બધા antiphishing સુવિધાઓ છોડવાની ભલામણ કરીએ છીએ';
+$lang['antiphishing_block_cloaked'] = 'બ્લોક ક્લોક્ડ URL';
+$lang['antiphishing_block_ssl_mismatch'] = 'SSL અકસ્માતને અવરોધિત કરો';
+$lang['antiphishing_heuristics_engine'] = 'હેરીસ્ટીક એન્જિન';
+$lang['antiphishing_signature_engine'] = 'સહી એન્જિન';
