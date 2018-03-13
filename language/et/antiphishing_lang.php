050be15642dc567c3ee267bf36f147de483697b2
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:01:14 +0000

    Add new files and directories, commit changes, push changes

--- language/et/antiphishing_lang.php
+++ language/et/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'The Gateway antipettuse app kasutab keskne antipettuse mootori skaneerida web, FTP, posti ja rohkem. See kaitseb seadmeid võrku ühendatud varje kasutajate pahatahtliku lingid avastatud mootor.';
-$lang['antiphishing_app_name'] = 'Gateway antipettuse';
-$lang['antiphishing_app_tooltip'] = 'Isegi kõige Savvy Internet kasutajad saavad kogemata klõpsa phishing link. Soovitame jätta kõik antipettuse funktsioone lubatud.';
-$lang['antiphishing_block_cloaked'] = 'Block Kaetud URL';
-$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL mismatch';
-$lang['antiphishing_heuristics_engine'] = 'heuristiline Mootor';
-$lang['antiphishing_signature_engine'] = 'allkiri Mootor';
+$lang['antiphishing_app_description'] = 'Gateway Antiphishing rakendus kasutab veebi, FTP, posti ja muu skannimiseks keskset antifootsusmootorit. See kaitseb teie võrguga ühendatud seadmeid, kaitstes kasutajaid mootori tuvastatud pahatahtlikest linkidest.';
+$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Isegi kõige kogenumad Interneti-kasutajad võivad kogemata klõpsata andmepüügiks. Soovitame jätta kõik antifailide funktsioonid välja.';
+$lang['antiphishing_block_cloaked'] = 'Blokeeri kaitstud URL-id';
+$lang['antiphishing_block_ssl_mismatch'] = 'Blokeeri SSL-i mittevastavus';
+$lang['antiphishing_heuristics_engine'] = 'Heuristiline mootor';
+$lang['antiphishing_signature_engine'] = 'Allkirja mootor';
