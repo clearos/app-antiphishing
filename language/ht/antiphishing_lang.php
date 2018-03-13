da5542d3c3aea511a6f800d1ddd892f9505e7ba5
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:05:38 +0000

    Add new files and directories, commit changes, push changes

--- language/ht/antiphishing_lang.php
+++ language/ht/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'App a Gateway antiphishing sèvi ak yon motè antiphishing santral nan analysis sou sit wèb ★ ftp, lapòs ak plis ankò. Li pwoteje aparèy konekte nan rezo ou pa pwoteksyon itilizatè yo soti nan lyen move detekte avèk motè a.';
-$lang['antiphishing_app_name'] = 'Gateway antiphishing';
-$lang['antiphishing_app_tooltip'] = 'Menm itilizatè sa yo sou Entènèt pi konprandr ka aksidantèlman klike sou yon lyen èskrokri. Nou rekòmande kite tout karakteristik antiphishing pèmèt.';
-$lang['antiphishing_block_cloaked'] = 'Blòk cloaked URL';
-$lang['antiphishing_block_ssl_mismatch'] = 'Blòk SSL Matche';
-$lang['antiphishing_heuristics_engine'] = 'eristik Engine';
-$lang['antiphishing_signature_engine'] = 'siyati Engine';
+$lang['antiphishing_app_description'] = 'App a Antiphishing Gateway sèvi ak yon santral antifriz motè yo scan entènèt, ftp, lapòs ak plis ankò. Li pwoteje aparèy ki konekte nan rezo ou pa pwoteje itilizatè yo soti nan lyen move detekte pa motè a.';
+$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Menm itilizatè yo entènèt ki pi konprandr ka aksidantèlman klike sou yon lyen èskrokri. Nou rekòmande kite tout karakteristik antiphishing pèmèt.';
+$lang['antiphishing_block_cloaked'] = 'Blòk Cloaked URL';
+$lang['antiphishing_block_ssl_mismatch'] = 'Blòk SSL Mismatch';
+$lang['antiphishing_heuristics_engine'] = 'Heuristic Engine';
+$lang['antiphishing_signature_engine'] = 'Siyati Engine';
