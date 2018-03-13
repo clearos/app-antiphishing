3441cb59546624c2925303c4d8ca248cd22077d1
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:20:39 +0000

    Add new files and directories, commit changes, push changes

--- language/mn/antiphishing_lang.php
+++ language/mn/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'Gateway Antiphishing апп вэб, FTP, шуудан, илүү скан төв antiphishing хөдөлгүүрийг ашигладаг. Энэ хамгаалсан хөдөлгүүртэй илэрсэн хортой холбоосууд хэрэглэгчдийг таны сүлжээнд холбогдсон төхөөрөмжийг хамгаалдаг.';
+$lang['antiphishing_app_description'] = 'Gateway Antiphishing app нь вэб, FTP, мэйл болон бусад сканнеруудыг сканнердах төв antiphishing хөдөлгүүрийг ашигладаг. Энэ нь таны сүлжээнд холбогдсон төхөөрөмжүүдийг хамгаалж, хөдөлгүүрээс илэрсэн хортой холбоосоос хэрэглэгчдийг хамгаалж хамгаална.';
 $lang['antiphishing_app_name'] = 'Gateway Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'Тэр ч байтугай хамгийн ухаантай Интернэт хэрэглэгчид санамсаргүй нь фишинг холбоос дээр дарна уу. Бид боломжтой бүх antiphishing боломжуудыг орхиж зөвлөж байна.';
-$lang['antiphishing_block_cloaked'] = 'Блок Cloaked URL-ууд';
-$lang['antiphishing_block_ssl_mismatch'] = 'Блок SSL таарахгүй';
-$lang['antiphishing_heuristics_engine'] = 'Сэтгэн олох Engine';
-$lang['antiphishing_signature_engine'] = 'Гарын үсэг Engine';
+$lang['antiphishing_app_tooltip'] = 'Интернет хамгийн ухаалаг Интернет хэрэглэгч ч гэсэн санамсаргүй байдлаар фишингийн холбоос дээр дарах боломжтой. Бид идэвхжүүлсэн бүх функцуудыг идэвхжүүлсэн байхыг зөвлөж байна.';
+$lang['antiphishing_block_cloaked'] = 'Блокоо нөмрөг URL';
+$lang['antiphishing_block_ssl_mismatch'] = 'SSL үл тохирох block';
+$lang['antiphishing_heuristics_engine'] = 'Heuristic Engine';
+$lang['antiphishing_signature_engine'] = 'Гарын үсгийн хөдөлгүүр';
