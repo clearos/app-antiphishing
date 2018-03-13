15443a19346d459b6f590699f9613ea1810f9161
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:04:16 +0000

    Add new files and directories, commit changes, push changes

--- language/el/antiphishing_lang.php
+++ language/el/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'Η εφαρμογή πύλη Antiphishing χρησιμοποιεί μια κεντρική μηχανή antiphishing να σαρώσετε web, FTP, ταχυδρομείο και πολλά άλλα. Προστατεύει τις συσκευές που συνδέονται στο δίκτυό σας με την θωράκιση των χρηστών από κακόβουλες συνδέσεις που ανιχνεύεται από τον κινητήρα.';
-$lang['antiphishing_app_name'] = 'πύλη Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'Ακόμη και οι πιο συνειδητοποιημένοι χρήστες του Διαδικτύου μπορούν κατά λάθος κλικ σε έναν σύνδεσμο phishing. Συστήνουμε αφήνοντας όλα τα χαρακτηριστικά antiphishing ενεργοποιημένη.';
-$lang['antiphishing_block_cloaked'] = 'Αποκλεισμός Cloaked διευθύνσεις URL';
-$lang['antiphishing_block_ssl_mismatch'] = 'Αποκλεισμός SSL Ασυμφωνία';
-$lang['antiphishing_heuristics_engine'] = 'ευρετική μηχανή';
-$lang['antiphishing_signature_engine'] = 'μηχανή υπογραφή';
+$lang['antiphishing_app_description'] = 'Η εφαρμογή Gatephile Antiphishing χρησιμοποιεί μια κεντρική μηχανή για την ανίχνευση του ιστού, για τη σάρωση ιστού, FTP, αλληλογραφίας και πολλά άλλα. Προστατεύει τις συσκευές που είναι συνδεδεμένες στο δίκτυό σας προστατεύοντας τους χρήστες από κακόβουλες συνδέσεις που εντοπίζονται από τον κινητήρα.';
+$lang['antiphishing_app_name'] = 'Gatephishing Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Ακόμη και οι πιο καταλαβανοί χρήστες του Διαδικτύου μπορούν να κάνουν κλικ σε έναν σύνδεσμο ηλεκτρονικού "ψαρέματος". Συνιστούμε να αφήνετε όλες τις λειτουργίες αντιφιλίωσης ενεργοποιημένες.';
+$lang['antiphishing_block_cloaked'] = 'Αποκλεισμός διευθύνσεων URL με επένδυση';
+$lang['antiphishing_block_ssl_mismatch'] = 'Αποκλεισμός αναντιστοιχίας SSL';
+$lang['antiphishing_heuristics_engine'] = 'Ευριστική μηχανή';
+$lang['antiphishing_signature_engine'] = 'Υπογραφή κινητήρα';
