08cd2d727ed4e732cdf4b737d3fc4d91feedca01
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:42:33 +0000

    Add new files and directories, commit changes, push changes

--- language/yo/antiphishing_lang.php
+++ language/yo/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'The Gateway Antiphishing app nlo a aringbungbun antiphishing engine lati ọlọjẹ ayelujara, FTP, mail ati siwaju sii. O aabo awọn ẹrọ ti sopọ si nẹtiwọki rẹ nipa shielding awọn olumulo lati irira ìjápọ ri nipa awọn engine.';
-$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'Paapa julọ sawy ayelujara awọn olumulo le lairotẹlẹ tẹ lori a ararẹ ọna asopọ. A ṣe iṣeduro nlọ gbogbo antiphishing awọn ẹya ara ẹrọ ṣiṣẹ.';
-$lang['antiphishing_block_cloaked'] = 'Àkọsílẹ Cloaked URL';
-$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL mismatch';
-$lang['antiphishing_heuristics_engine'] = 'oni wakati engine';
-$lang['antiphishing_signature_engine'] = 'Ibuwọlu engine';
+$lang['antiphishing_app_description'] = 'Ẹrọ Ṣiṣeto ti Gateway Antiphishing nlo engine ti o niiṣiro lati ṣawari wẹẹbu, FTP, mail ati diẹ sii. O ndaabobo awọn ẹrọ ti a ti sopọ si nẹtiwọki rẹ nipasẹ dabobo awọn olumulo lati irira-ọna ti o mọ nipasẹ ẹrọ.';
+$lang['antiphishing_app_name'] = 'Ṣiṣe-ọna ẹnu-ọna Gateway';
+$lang['antiphishing_app_tooltip'] = 'Paapa awọn olumulo Ayelujara ti o mọ julọ julọ le tẹ aifọwọyi tẹ lori ọna asopọ-ararẹ. A ṣe iṣeduro lati fi gbogbo awọn ẹya antiphishing ṣiṣẹ.';
+$lang['antiphishing_block_cloaked'] = 'Block Cloaked URLs';
+$lang['antiphishing_block_ssl_mismatch'] = 'Ṣiṣeto Iṣura SSL';
+$lang['antiphishing_heuristics_engine'] = 'Heuristic Engine';
+$lang['antiphishing_signature_engine'] = 'Ibuwọlu Ibuwọlu';
