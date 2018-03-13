a75928a302cd0cf321ea3114c3be67a7d3a088d2
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:15:37 +0000

    Add new files and directories, commit changes, push changes

--- language/la/antiphishing_lang.php
+++ language/la/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'Praesent Antiphishing App Engine et scan in telam ponit medium Antiphishing, FTP, et mail. Quod protegit tua fabrica coniunctum network users per protegens de Engine maligna links deprehenditur.';
-$lang['antiphishing_app_name'] = 'Antiphishing Gateway';
-$lang['antiphishing_app_tooltip'] = 'Sed savvy maxime Internet users click per accidens, non potest in links a phishing. We recommend relictis omnibus features Antiphishing enabled.';
-$lang['antiphishing_block_cloaked'] = 'Block Cloaked URLs';
-$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL Mismatch';
-$lang['antiphishing_heuristics_engine'] = 'Vivamus heuristic';
-$lang['antiphishing_signature_engine'] = 'signature Engine';
+$lang['antiphishing_app_description'] = 'Antiphishing app quod utitur porta media Antiphishing engine scan in telam, FTP, et mail. Protegit a consiliis malitiae coniunctum network users per nexus protegens deprehendi machinam.';
+$lang['antiphishing_app_name'] = 'porta Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Internet users potest etiam per accidens most savvy hamatae click in link. Te admonemus ut relinquens omnia Antiphishing features enabled.';
+$lang['antiphishing_block_cloaked'] = 'Cloaked delata obstructionum';
+$lang['antiphishing_block_ssl_mismatch'] = 'SSL Mismatch obstructionum';
+$lang['antiphishing_heuristics_engine'] = 'heuristic Engine';
+$lang['antiphishing_signature_engine'] = 'subscriptio Engine';
