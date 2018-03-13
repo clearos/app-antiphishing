0fa4831b052975675a6feeb857d54e6c7a4e80f4
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:06:29 +0000

    Add new files and directories, commit changes, push changes

--- language/ha/antiphishing_lang.php
+++ language/ha/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'The Gateway Antiphishing app yana amfani da wani tsakiya antiphishing engine to duba yanar gizo, FTP, mail da kuma more. Yana kare na&#39;urorin da alaka to your cibiyar sadarwa da da tsare masu amfani daga qeta links gano da engine.';
-$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'Ko da ya fi savvy Internet masu amfani iya bazata danna kan wani mai leƙan asirri link. Muna bada shawara a bar dukan antiphishing fasali sa.';
+$lang['antiphishing_app_description'] = 'Kayan gogewa na Ƙarin Bayani yana amfani da injiniya na tsakiya don duba yanar gizo, FTP, imel da sauransu. Yana kare na&#39;urorin da aka haɗa zuwa cibiyar sadarwar ku ta hanyar kare masu amfani daga magungunan haɗi da injiniyar ta gano.';
+$lang['antiphishing_app_name'] = 'Ƙofafiyar Ƙarfafawa';
+$lang['antiphishing_app_tooltip'] = 'Ko da mafi yawan masu amfani da intanit na Intanet za su iya danna kan hanyar haɗin phishing. Muna bada shawara barin dukan fasalulluran siffofi.';
 $lang['antiphishing_block_cloaked'] = 'Block Cloaked URLs';
-$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL mismatch';
+$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL Mismatch';
 $lang['antiphishing_heuristics_engine'] = 'Heuristic Engine';
-$lang['antiphishing_signature_engine'] = 'Sa hannu Engine';
+$lang['antiphishing_signature_engine'] = 'Injin Wuta';
