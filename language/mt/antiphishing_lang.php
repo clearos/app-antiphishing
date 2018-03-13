2a6d68737b6759e11b0c254ab032bbeba1a62402
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:22:19 +0000

    Add new files and directories, commit changes, push changes

--- language/mt/antiphishing_lang.php
+++ language/mt/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'Il-app Bieb antiphishing juża magna antiphishing ċentrali jiġu skennjati web, FTP, posta u aktar. Jipproteġi mezzi konnessi man-network tiegħek billi jħarisha utenti minn rabtiet malizzjużi skoperti mill-magna.';
-$lang['antiphishing_app_name'] = 'Gateway antiphishing';
-$lang['antiphishing_app_tooltip'] = 'Anki l-utenti l-Internet aktar sofistikati jistgħu aċċidentalment ikklikkja fuq rabta phishing. Nirrakkomandaw li jħallu l-karatteristiċi kollha antiphishing ppermettiet.';
-$lang['antiphishing_block_cloaked'] = 'Blokk cloaked URLs';
-$lang['antiphishing_block_ssl_mismatch'] = 'Blokk SSL diskrepanza';
-$lang['antiphishing_heuristics_engine'] = 'magna heuristic';
-$lang['antiphishing_signature_engine'] = 'firma tal-magna';
+$lang['antiphishing_app_description'] = 'L-applikazzjoni ta &#39;Gateway Antiphishing tuża magna ċentrali kontra s-sogħba biex tiskannja l-internet, FTP, posta u aktar. Tipproteġi l-mezzi konnessi man-network tiegħek billi tħares lill-utenti minn konnessjonijiet malizzjużi skoperti mill-magna.';
+$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Anki l-utenti l-aktar sofistikati ta &#39;l-Internet jistgħu aċċidentalment ikklikkjaw fuq link ta&#39; phising. Nirrakkomandaw li jħallu l-karatteristiċi antiphishing kollha ppermettiet.';
+$lang['antiphishing_block_cloaked'] = 'Blokk URLs imqaxxra';
+$lang['antiphishing_block_ssl_mismatch'] = 'Imblukkar tal-blokk SSL';
+$lang['antiphishing_heuristics_engine'] = 'Magna heuristika';
+$lang['antiphishing_signature_engine'] = 'Magna tal-Firma';
