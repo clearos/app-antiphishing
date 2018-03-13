ebb9da28a9f17fc8752a17c4db8d9216be6542ee
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:19:26 +0000

    Add new files and directories, commit changes, push changes

--- language/mk/antiphishing_lang.php
+++ language/mk/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'апликација Портал Antiphishing користи централна antiphishing моторот за скенирање на веб, FTP, пошта и повеќе. Таа го заштитува уреди поврзани на вашата мрежа со заштитен корисниците од малициозни линкови откриени од страна на моторот.';
-$lang['antiphishing_app_name'] = 'портал Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'Дури и повеќето такт интернет корисници случајно може да кликнете на линкот за phishing. Ние препорачуваме оставајќи ги сите antiphishing овозможено можности.';
-$lang['antiphishing_block_cloaked'] = 'Забрани пелерина адреси';
-$lang['antiphishing_block_ssl_mismatch'] = 'Забрани SSL Несогласување';
-$lang['antiphishing_heuristics_engine'] = 'хеуристична';
-$lang['antiphishing_signature_engine'] = 'потпис на мотор';
+$lang['antiphishing_app_description'] = 'Апликацијата за пристап на портали користи централен антифризиран мотор за скенирање на веб, FTP, пошта и повеќе. Ги штити уредите поврзани со вашата мрежа со заштита на корисниците од малициозни линкови откриени од моторот.';
+$lang['antiphishing_app_name'] = 'Портален пресек';
+$lang['antiphishing_app_tooltip'] = 'Дури и најкористен интернет корисниците случајно можат да кликнат на врската со фишинг. Препорачуваме да ги оставите сите антифриз функции овозможени.';
+$lang['antiphishing_block_cloaked'] = 'Блокирани URL адреси';
+$lang['antiphishing_block_ssl_mismatch'] = 'Блокирај SSL Несогласување';
+$lang['antiphishing_heuristics_engine'] = 'Хеуристички мотор';
+$lang['antiphishing_signature_engine'] = 'Потпис мотор';
