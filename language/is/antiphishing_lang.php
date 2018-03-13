ddc0a71da9f5eab66cbe705141013db62d38b28e
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:10:32 +0000

    Add new files and directories, commit changes, push changes

--- language/is/antiphishing_lang.php
+++ language/is/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'The Gateway Antiphishing app notar miðlæga antiphishing vél til að skanna vefinn, FTP, tölvupóst og fleira. Það ver tæki tengd við netið til að hlífa notendur frá illgjarn tenglar uppgötva við the vél.';
+$lang['antiphishing_app_description'] = 'The Gateway Antiphishing app notar aðal antiphishing vél til að skanna vefur, FTP, póst og fleira. Það verndar tæki sem tengjast netkerfinu þínu með því að verja notendur frá illgjarnum tenglum sem vélin kemst að.';
 $lang['antiphishing_app_name'] = 'Gateway Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'Jafnvel kunnátta notandi getur tilviljun smellt á phishing tengilinn. Við mælum með að fara alla antiphishing eiginleika virka.';
-$lang['antiphishing_block_cloaked'] = 'Blokk Cloaked vefslóðir';
-$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL Misræmi';
-$lang['antiphishing_heuristics_engine'] = 'leitandi Engine';
-$lang['antiphishing_signature_engine'] = 'undirskrift Engine';
+$lang['antiphishing_app_tooltip'] = 'Jafnvel flestir kunnátta netnotendur geta óvart smellt á phishing tengil. Við mælum með því að láta allar antiphishing aðgerðir virkt.';
+$lang['antiphishing_block_cloaked'] = 'Lokaðu útilokuðu vefslóðum';
+$lang['antiphishing_block_ssl_mismatch'] = 'Lokaðu SSL frávik';
+$lang['antiphishing_heuristics_engine'] = 'Heuristic Engine';
+$lang['antiphishing_signature_engine'] = 'Undirskriftarvél';
