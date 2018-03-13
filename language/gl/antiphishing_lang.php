dc6ded8b62ec1a5dfc2ebe9b6c8595c1624243e6
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:02:55 +0000

    Add new files and directories, commit changes, push changes

--- language/gl/antiphishing_lang.php
+++ language/gl/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'A aplicación de pasarela Antiphishing usa un motor antiphishing central para dixitalizar web, FTP, correo electrónico e moito máis. El protexe os dispositivos conectados á súa rede, protexendo os usuarios de enlaces maliciosos detectados polo mecanismo.';
-$lang['antiphishing_app_name'] = 'pasarela Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'Mesmo os usuarios de Internet máis expertos poden accidentalmente facer clic nun enlace de phishing. Recomendamos deixar todos os recursos antiphishing activado.';
-$lang['antiphishing_block_cloaked'] = 'URL bloque Cloaked';
-$lang['antiphishing_block_ssl_mismatch'] = 'Bloque SSL Incompatibilidade';
-$lang['antiphishing_heuristics_engine'] = 'motor heurístico';
-$lang['antiphishing_signature_engine'] = 'Motor de subscrición';
+$lang['antiphishing_app_description'] = 'A aplicación Gateway Antiphishing usa un motor central antiphishing para escanear web, FTP, correo electrónico e moito máis. Protexe dispositivos conectados á rede protexendo aos usuarios das ligazóns maliciosas detectadas polo motor.';
+$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Incluso os usuarios máis expertos de Internet poden premer accidentalmente nunha ligazón de phishing. Recomendamos deixar todas as funcións antiphishing activadas.';
+$lang['antiphishing_block_cloaked'] = 'Bloquear os URL encapsulados';
+$lang['antiphishing_block_ssl_mismatch'] = 'Bloquear SSL Non coincidindo';
+$lang['antiphishing_heuristics_engine'] = 'Motor Heurístico';
+$lang['antiphishing_signature_engine'] = 'Motor de sinatura';
