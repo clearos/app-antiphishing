a0dcbfee6dde12605c965337a79b8701a7db8b11
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:14:45 +0000

    Add new files and directories, commit changes, push changes

--- language/ko/antiphishing_lang.php
+++ language/ko/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = '게이트웨이 안티 피싱 앱은 웹, FTP, 메일 등을 스캔 중앙 안티 피싱 엔진을 사용합니다. 이 엔진에 의해 감지 된 악성 링크에서 사용자를 차단하여 네트워크에 연결된 장치를 보호합니다.';
+$lang['antiphishing_app_description'] = 'Gateway Antiphishing 앱은 중앙 안티 피싱 엔진을 사용하여 웹, FTP, 메일 등을 검색합니다. 엔진이 탐지 한 악성 링크로부터 사용자를 보호하여 네트워크에 연결된 장치를 보호합니다.';
 $lang['antiphishing_app_name'] = '게이트웨이 안티 피싱';
-$lang['antiphishing_app_tooltip'] = '심지어 가장 잘 아는 인터넷 사용자가 실수로 피싱 링크를 클릭 할 수 있습니다. 우리가 사용하는 모든 안티 피싱 기능을 남겨 두는 것이 좋습니다.';
-$lang['antiphishing_block_cloaked'] = '블록 은폐 된 URL';
-$lang['antiphishing_block_ssl_mismatch'] = '블록 SSL 불일치';
-$lang['antiphishing_heuristics_engine'] = '휴리스틱 엔진';
+$lang['antiphishing_app_tooltip'] = '가장 잘 아는 인터넷 사용자조차도 우연히 피싱 링크를 클릭 할 수 있습니다. 모든 피싱 방지 기능을 사용하도록 설정하는 것이 좋습니다.';
+$lang['antiphishing_block_cloaked'] = '클로킹 된 URL 차단';
+$lang['antiphishing_block_ssl_mismatch'] = 'SSL 불일치 차단';
+$lang['antiphishing_heuristics_engine'] = '발견 적 엔진';
 $lang['antiphishing_signature_engine'] = '서명 엔진';
