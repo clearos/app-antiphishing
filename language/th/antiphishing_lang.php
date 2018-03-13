52f50ba661b20e7f863cdfde37014eeb97101465
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:37:04 +0000

    Add new files and directories, commit changes, push changes

--- language/th/antiphishing_lang.php
+++ language/th/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'แอปพลิเคเกตเวย์ Antiphishing ใช้เครื่องยนต์ antiphishing กลางที่จะสแกนเว็บ, FTP, อีเมลและอื่น ๆ จะช่วยปกป้องอุปกรณ์ที่เชื่อมต่อกับเครือข่ายของคุณโดยการป้องกันผู้ใช้จากลิงก์ที่เป็นอันตรายที่ตรวจพบโดยเครื่องยนต์';
-$lang['antiphishing_app_name'] = 'เกตเวย์ Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'แม้ผู้ใช้อินเทอร์เน็ตเข้าใจมากที่สุดตั้งใจสามารถคลิกที่ลิงค์ฟิชชิ่ง เราขอแนะนำให้ออกจากคุณลักษณะ antiphishing เปิดใช้งานทั้งหมด';
-$lang['antiphishing_block_cloaked'] = 'บล็อก URL ปิดบัง';
-$lang['antiphishing_block_ssl_mismatch'] = 'บล็อก SSL ไม่ตรงกัน';
-$lang['antiphishing_heuristics_engine'] = 'เครื่องยนต์ Heuristic';
+$lang['antiphishing_app_description'] = 'แอ็พพลิเคชัน Gateway Antiphishing ใช้เครื่องมือ antiphishing ส่วนกลางเพื่อสแกนเว็บ FTP และจดหมายและอื่น ๆ ช่วยปกป้องอุปกรณ์ที่เชื่อมต่อกับเครือข่ายของคุณโดยป้องกันผู้ใช้จากลิงก์ที่เป็นอันตรายซึ่งตรวจพบโดยเครื่องยนต์';
+$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'แม้แต่ผู้ใช้อินเทอร์เน็ตที่เข้าใจง่ายที่สุดก็สามารถคลิกที่ลิงก์ฟิชชิ่งได้ เราขอแนะนำให้เปิดใช้คุณลักษณะ antiphishing ทั้งหมดไว้';
+$lang['antiphishing_block_cloaked'] = 'ปิดกั้น URL ที่ปิดบังไว้';
+$lang['antiphishing_block_ssl_mismatch'] = 'ป้องกันไม่ให้ SSL ไม่ตรงกัน';
+$lang['antiphishing_heuristics_engine'] = 'Heuristic Engine';
 $lang['antiphishing_signature_engine'] = 'เครื่องยนต์ลายเซ็น';
