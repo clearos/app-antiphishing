56420b7edbc6a61f037c0dab871bf8ebf8b0a5b2
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:23:34 +0000

    Add new files and directories, commit changes, push changes

--- language/nl/antiphishing_lang.php
+++ language/nl/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'The Gateway Antiphishing app maakt gebruik van een centrale antiphishing-engine om web, FTP, e-mail en nog veel meer scannen. Het beschermt apparaten die zijn aangesloten op uw netwerk door het afschermen van gebruikers tegen kwaadaardige koppelingen gedetecteerd door de motor.';
-$lang['antiphishing_app_name'] = 'gateway Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'Zelfs de meest snuggere Internet gebruikers kunnen per ongeluk op een phishing-link. We raden u aan alle antiphishing functies ingeschakeld.';
-$lang['antiphishing_block_cloaked'] = 'Block Cloaked URLs';
-$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL Mismatch';
-$lang['antiphishing_heuristics_engine'] = 'Heuristic Engine';
-$lang['antiphishing_signature_engine'] = 'handtekening Engine';
+$lang['antiphishing_app_description'] = 'De Gateway Antiphishing-app gebruikt een centrale antiphishing-engine voor het scannen van web, FTP, e-mail en meer. Het beschermt apparaten die op uw netwerk zijn aangesloten door gebruikers te beschermen tegen schadelijke koppelingen die door de motor worden gedetecteerd.';
+$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Zelfs de meest slimme internetgebruikers kunnen per ongeluk op een phishing-link klikken. We raden aan om alle antiphishing-functies ingeschakeld te laten.';
+$lang['antiphishing_block_cloaked'] = 'Block Cloaked URL&#39;s';
+$lang['antiphishing_block_ssl_mismatch'] = 'SSL-mismatch blokkeren';
+$lang['antiphishing_heuristics_engine'] = 'Heuristische motor';
+$lang['antiphishing_signature_engine'] = 'Handtekening motor';
