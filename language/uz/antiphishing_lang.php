495a5cffde11025db814135ed82341eeb788b075
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:40:02 +0000

    Add new files and directories, commit changes, push changes

--- language/uz/antiphishing_lang.php
+++ language/uz/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'Gateway antiphishing App veb-, FTP, pochta va yana harakatlanish uchun markaziy antiphishing qidiruvi foydalanadi. Bu vosita tomonidan aniqlangan zararli ulanish foydalanuvchilarning himoya bilan tarmoqqa ulangan qurilmalar himoya qiladi.';
-$lang['antiphishing_app_name'] = 'Gateway antiphishing';
-$lang['antiphishing_app_tooltip'] = 'Hatto, eng tushunadigan Internet foydalanuvchilari tasodifan bir fishing tugmasini bosing mumkin. Biz yoqilgan barcha antiphishing xususiyatlarini qoldirib tavsiya qilamiz.';
-$lang['antiphishing_block_cloaked'] = 'Blok yashirin URL';
-$lang['antiphishing_block_ssl_mismatch'] = 'Blok SSL turi mos kelmadi';
-$lang['antiphishing_heuristics_engine'] = 'evristik Engine';
-$lang['antiphishing_signature_engine'] = 'Imzo Engine';
+$lang['antiphishing_app_description'] = 'Gateway Antiphishing ilovasi veb, FTP, pochta va boshqa narsalarni skanerlash uchun markaziy antiphishing qidiruvi ishlatadi. Bu foydalanuvchilarni dvigatel tomonidan aniqlangan zararli aloqalardan himoyalash orqali tarmoqqa ulangan qurilmalarni himoya qiladi.';
+$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Hatto eng g&#39;ayrioddiy Internet foydalanuvchilari tasodifan phishing havolasini bosing. Barcha antiphishing xususiyatlarini yoqib qo&#39;yishni tavsiya etamiz.';
+$lang['antiphishing_block_cloaked'] = 'Qo&#39;yilgan URL manzillarini bloklash';
+$lang['antiphishing_block_ssl_mismatch'] = 'SSL nosozligini bloklash';
+$lang['antiphishing_heuristics_engine'] = 'Sezuvchan mexanizm';
+$lang['antiphishing_signature_engine'] = 'Imzo vositasi';
