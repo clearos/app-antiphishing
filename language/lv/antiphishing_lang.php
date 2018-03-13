4454922fb5e90e382b1cb563ac78b467681864ff
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:17:19 +0000

    Add new files and directories, commit changes, push changes

--- language/lv/antiphishing_lang.php
+++ language/lv/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'Gateway Antiphishing lietotne izmanto centrālā antiphishing dzinēju, lai skenētu mājas, FTP, pasta un vairāk. Tas aizsargā ierīces, kas saistīts ar jūsu tīklu, pasargājot lietotājus no ļaunprātīgas saites atklāto motora.';
+$lang['antiphishing_app_description'] = 'Programmā Gateway Antiphishing tiek izmantots galvenais antiphishing rīks, lai skenētu tīmekli, FTP, pastu un citus. Tas aizsargā jūsu tīklā pieslēgtās ierīces, pasargājot lietotājus no motora apzinātām ļaunprātīgām saitēm.';
 $lang['antiphishing_app_name'] = 'Gateway Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'Pat visvairāk savvy interneta lietotāji var nejauši noklikšķināt uz pikšķerēšanas saites. Mēs iesakām atstājot visus Antiphishing iezīmes ļāva.';
-$lang['antiphishing_block_cloaked'] = 'Bloķēt Cloaked URL';
-$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL neatbilstība';
-$lang['antiphishing_heuristics_engine'] = 'heiristisko Dzinējs';
-$lang['antiphishing_signature_engine'] = 'paraksts Dzinējs';
+$lang['antiphishing_app_tooltip'] = 'Pat visdrošākie interneta lietotāji var nejauši noklikšķināt uz pikšķerēšanas saites. Mēs iesakām izslēgt visas antiphishing funkcijas.';
+$lang['antiphishing_block_cloaked'] = 'Bloķēt lokanīgus URL';
+$lang['antiphishing_block_ssl_mismatch'] = 'Bloķēt SSL nesakritību';
+$lang['antiphishing_heuristics_engine'] = 'Heiristiskais dzinējs';
+$lang['antiphishing_signature_engine'] = 'Paraksta dzinējs';
