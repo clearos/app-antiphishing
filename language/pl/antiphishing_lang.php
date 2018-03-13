ddc86e77f4c46fb6bf6005d55359d51f1e41ee74
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:26:03 +0000

    Add new files and directories, commit changes, push changes

--- language/pl/antiphishing_lang.php
+++ language/pl/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'Aplikacja Brama Antyphishing wykorzystuje centralny silnik antyphishingową skanować WWW, FTP, poczta i inne. Chroni urządzenie podłączone do sieci przez ekranowanie użytkowników przed złośliwymi linkami wykrytych przez silnik.';
-$lang['antiphishing_app_name'] = 'bramka Antyphishing';
-$lang['antiphishing_app_tooltip'] = 'Nawet najbardziej doświadczeni internauci mogą przypadkowo kliknąć na link phishing. Zalecamy pozostawienie wszystkich funkcji Antyphishing włączona.';
-$lang['antiphishing_block_cloaked'] = 'Blokowanie URL maskowana';
-$lang['antiphishing_block_ssl_mismatch'] = 'Blok SSL Niedopasowanie';
-$lang['antiphishing_heuristics_engine'] = 'heurystyczna';
-$lang['antiphishing_signature_engine'] = 'podpis Silnik';
+$lang['antiphishing_app_description'] = 'Aplikacja Gateway Antyphishing wykorzystuje centralny mechanizm antyphishingowy do skanowania stron internetowych, FTP, poczty i innych. Chroni urządzenia podłączone do sieci, chroniąc użytkowników przed złośliwymi łączami wykrytymi przez silnik.';
+$lang['antiphishing_app_name'] = 'Gate Antyphishing';
+$lang['antiphishing_app_tooltip'] = 'Nawet najbardziej doświadczeni internauci mogą przypadkowo kliknąć link phishingowy. Zalecamy pozostawienie włączonych wszystkich funkcji antyphishingowych.';
+$lang['antiphishing_block_cloaked'] = 'Blokuj zamaskowane adresy URL';
+$lang['antiphishing_block_ssl_mismatch'] = 'Zablokuj niedopasowanie protokołu SSL';
+$lang['antiphishing_heuristics_engine'] = 'Heuristic Engine';
+$lang['antiphishing_signature_engine'] = 'Podpis silnika';
