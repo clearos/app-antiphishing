4d65b47ba9538cbefddcf753b283c46572ca15a0
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:39:36 +0000

    Add new files and directories, commit changes, push changes

--- language/ur/antiphishing_lang.php
+++ language/ur/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'گیٹ وے Antiphishing اے پی پی کی ویب، FTP، میل اور زیادہ اسکین کرنے کے ایک مرکزی antiphishing انجن کا استعمال کرتا. اس کے انجن کی طرف سے پتہ درنساوناپورن لنکس سے صارفین کو بچانے کی طرف سے آپ کے نیٹ ورک سے منسلک آلات کی حفاظت کرتا ہے.';
-$lang['antiphishing_app_name'] = 'گیٹ وے Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'یہاں تک کہ سب سے زیادہ پریمی انٹرنیٹ صارفین اتفاقی طور پر ایک فریب دہی کے لنک پر کلک کر سکتے ہیں. ہم فعال تمام antiphishing خصوصیات کو چھوڑ سفارش کرتے ہیں.';
-$lang['antiphishing_block_cloaked'] = 'بلاک Al-Muddaththir شروع کے URLs';
-$lang['antiphishing_block_ssl_mismatch'] = 'بلاک SSL غیرمشابہ';
-$lang['antiphishing_heuristics_engine'] = 'انکشافی انجن';
+$lang['antiphishing_app_description'] = 'گیٹ وے اینٹیفائسنگ اے پی پی ویب، FTP، میل اور زیادہ اسکین کرنے کے لئے مرکزی اینٹیفائش انجن کا استعمال کرتا ہے. یہ انجن کے ذریعے دریافت کردہ بدسلوکی لنکس سے صارفین کو بچانے کے ذریعے اپنے نیٹ ورک سے منسلک آلات کی حفاظت کرتا ہے.';
+$lang['antiphishing_app_name'] = 'گیٹ وے اینٹیفائنگ';
+$lang['antiphishing_app_tooltip'] = 'یہاں تک کہ سب سے زیادہ پریمی انٹرنیٹ کے صارفین کو غلطی سے ایک فشنگ ماہی گیری پر کلک کر سکتے ہیں. ہم تمام اینٹیفائشی خصوصیات کو چھوڑنے کی اجازت دیتے ہیں.';
+$lang['antiphishing_block_cloaked'] = 'بلاک Cloaked یو آر ایل';
+$lang['antiphishing_block_ssl_mismatch'] = 'بلاک ایس ایس ایل خرابی';
+$lang['antiphishing_heuristics_engine'] = 'حیاتیاتی انجن';
 $lang['antiphishing_signature_engine'] = 'دستخط انجن';
