bf44393f1399573d1f000fbdd934e74b4115052e
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:25:38 +0000

    Add new files and directories, commit changes, push changes

--- language/fa/antiphishing_lang.php
+++ language/fa/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'برنامه دروازه آنتی فیشینگ استفاده از یک موتور آنتی فیشینگ مرکزی برای اسکن وب، FTP، ایمیل و بیشتر. این دستگاه توسط کاربران را از لینک های مخرب شناسایی شده توسط موتور به شبکه شما متصل محافظت می کند.';
-$lang['antiphishing_app_name'] = 'دروازه آنتی فیشینگ';
-$lang['antiphishing_app_tooltip'] = 'حتی کاربران اینترنت زرنگ و دانا ترین به طور تصادفی می توانید بر روی لینک های فیشینگ را کلیک کنید. ما توصیه می کنیم از ترک تمام ویژگی های آنتی فیشینگ را فعال کنید.';
-$lang['antiphishing_block_cloaked'] = 'بلوک پردهی آدرس ها';
-$lang['antiphishing_block_ssl_mismatch'] = 'بلوک SSL عدم تطابق';
-$lang['antiphishing_heuristics_engine'] = 'موتور اکتشافی';
-$lang['antiphishing_signature_engine'] = 'امضاء موتور';
+$lang['antiphishing_app_description'] = 'برنامه Antiphishing Gateway با استفاده از یک موتور آنتی فیشینگ مرکزی برای اسکن وب، FTP، پست الکترونیکی و موارد دیگر. این دستگاه محافظت از دستگاه های متصل به شبکه شما را با محافظت کاربران از لینک های مخرب شناسایی شده توسط موتور.';
+$lang['antiphishing_app_name'] = 'دروازه هشدار';
+$lang['antiphishing_app_tooltip'] = 'حتی کاربران باهوش ترین اینترنت می توانند به صورت تصادفی بر روی لینک فیشینگ کلیک کنند. توصیه می کنیم همه ویژگی های ضد فیشینگ را فعال کنید.';
+$lang['antiphishing_block_cloaked'] = 'مسدود کردن URL های مسدود شده';
+$lang['antiphishing_block_ssl_mismatch'] = 'مسدود کردن SSL نامعتبر است';
+$lang['antiphishing_heuristics_engine'] = 'موتور هورستیک';
+$lang['antiphishing_signature_engine'] = 'موتور امضا';
