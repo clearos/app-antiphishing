c45d4f590249911e0e4857a980ce86ef389e4800
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:00:22 +0000

    Add new files and directories, commit changes, push changes

--- language/eo/antiphishing_lang.php
+++ language/eo/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'La Enirejo Antiphishing app uzas centran antiphishing motoro skani retejo, FTP, retpoŝto kaj pli. Ĝi protektas mekanismoj konektitaj al via reto per ŝirmado uzantoj de malicaj ligoj detektitaj de la motoro.';
-$lang['antiphishing_app_name'] = 'enirejo Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'Eĉ la plej sagacaj retumantoj povas hazarde alklaku phishing ligilon. Ni rekomendas lasi ĉiuj antiphishing karakterizaĵoj ebligita.';
-$lang['antiphishing_block_cloaked'] = 'Bloko cloaked URLoj';
-$lang['antiphishing_block_ssl_mismatch'] = 'Bloko SSL mismatch';
-$lang['antiphishing_heuristics_engine'] = 'heŭristiko Motoro';
-$lang['antiphishing_signature_engine'] = 'signature Motoro';
+$lang['antiphishing_app_description'] = 'La Gateway Antiphishing-programo uzas centran antifisan motoron por skani retejon, FTP, retpoŝton kaj pli. Ĝi protektas aparatojn konektitajn al via reto ŝirmante uzantojn de malicaj ligoj detektitaj de la motoro.';
+$lang['antiphishing_app_name'] = 'Pordejo Antipiŝanta';
+$lang['antiphishing_app_tooltip'] = 'Eĉ la plej saĝaj Interretaj uzantoj povas hazarde alklaki fiŝan ligilon. Ni rekomendas lasi ĉiujn antipiŝajn funkciojn ebligitaj.';
+$lang['antiphishing_block_cloaked'] = 'Bloku Klakitajn URLojn';
+$lang['antiphishing_block_ssl_mismatch'] = 'Bloko SSL-Mismatch';
+$lang['antiphishing_heuristics_engine'] = 'Heurisma Motoro';
+$lang['antiphishing_signature_engine'] = 'Signature Engine';
