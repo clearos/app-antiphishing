dfd4ed0437f549cf33fcb22769111165985254fa
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:11:48 +0000

    Add new files and directories, commit changes, push changes

--- language/ja/antiphishing_lang.php
+++ language/ja/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'ゲートウェイアンチフィッシングアプリは、Web、FTP、メールなどをスキャンするために、中央フィッシング対策エンジンを使用しています。 これは、エンジンによって検出された悪質なリンクからユーザーを保護することによって、ネットワークに接続された機器を保護します。';
+$lang['antiphishing_app_description'] = 'Gateway Antiphishingアプリケーションは、中央のアンチフィッシングエンジンを使用してWeb、FTP、メールなどをスキャンします。 これは、エンジンによって検出された悪質なリンクからユーザを保護することによって、ネットワークに接続されたデバイスを保護します。';
 $lang['antiphishing_app_name'] = 'ゲートウェイアンチフィッシング';
-$lang['antiphishing_app_tooltip'] = '最も精通したインターネットユーザーは誤ってフィッシングリンクをクリックすることができます。 私たちは有効になってすべてのフィッシング対策機能を残すことをお勧めします。';
-$lang['antiphishing_block_cloaked'] = 'ブロッククロークのURL';
-$lang['antiphishing_block_ssl_mismatch'] = 'ブロックSSLの不一致';
-$lang['antiphishing_heuristics_engine'] = 'ヒューリスティックエンジン';
-$lang['antiphishing_signature_engine'] = 'シグニチャエンジン';
+$lang['antiphishing_app_tooltip'] = '最も精通したインターネットユーザーであっても、誤ってフィッシングリンクをクリックする可能性があります。 すべてのアンチフィッシング機能を有効にしておくことをお勧めします。';
+$lang['antiphishing_block_cloaked'] = 'クローキングされたURLをブロックする';
+$lang['antiphishing_block_ssl_mismatch'] = 'SSLの不一致をブロックする';
+$lang['antiphishing_heuristics_engine'] = 'ヒューリスティックなエンジン';
+$lang['antiphishing_signature_engine'] = '署名エンジン';
