d41267361deb381e32cba58a33ad9176cf14ed5d
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:06:54 +0000

    Add new files and directories, commit changes, push changes

--- language/hi/antiphishing_lang.php
+++ language/hi/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'गेटवे Antiphishing एप्लिकेशन वेब, एफ़टीपी, मेल और अधिक स्कैन करने के लिए एक केंद्रीय AntiPhishing इंजन का उपयोग करता है। यह इंजन द्वारा पता लगाया दुर्भावनापूर्ण लिंक से उपयोगकर्ताओं को बचाने के द्वारा अपने नेटवर्क से जुड़े उपकरणों की रक्षा करता है।';
-$lang['antiphishing_app_name'] = 'गेटवे Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'यहां तक ​​कि सबसे समझ रखने वाले इंटरनेट उपयोगकर्ताओं को गलती से एक फिशिंग लिंक पर क्लिक कर सकते हैं। हम सभी AntiPhishing सुविधाएँ सक्षम छोड़ने की सलाह देते।';
-$lang['antiphishing_block_cloaked'] = 'ब्लॉक Cloaked यूआरएल';
-$lang['antiphishing_block_ssl_mismatch'] = 'ब्लॉक एसएसएल बेमेल';
-$lang['antiphishing_heuristics_engine'] = 'अनुमानी इंजन';
+$lang['antiphishing_app_description'] = 'गेटवे एंटीफ़िशिंग ऐप वेब, एफ़टीपी, मेल और अधिक स्कैन करने के लिए एक केंद्रीय एंटीफ़िशिंग इंजन का उपयोग करता है। यह उपयोगकर्ताओं द्वारा इंजन द्वारा की जा रही दुर्भावनापूर्ण लिंक से परिरक्षित करके अपने नेटवर्क से जुड़े उपकरणों की सुरक्षा करता है।';
+$lang['antiphishing_app_name'] = 'गेटवे एंटीफिशिंग';
+$lang['antiphishing_app_tooltip'] = 'यहां तक ​​कि सबसे अधिक समझदार इंटरनेट उपयोगकर्ता गलती से फ़िशिंग लिंक पर क्लिक कर सकते हैं। हम सभी एंटीफ़िशिंग फीचर्स सक्षम छोड़ने की सलाह देते हैं';
+$lang['antiphishing_block_cloaked'] = 'ब्लॉक किए गए URL ब्लॉक करें';
+$lang['antiphishing_block_ssl_mismatch'] = 'SSL बेमेल को अवरोधित करें';
+$lang['antiphishing_heuristics_engine'] = 'हेरिस्टिक इंजन';
 $lang['antiphishing_signature_engine'] = 'हस्ताक्षर इंजन';
