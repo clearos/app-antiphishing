31a5213840f8d9d43fbd2670d2825e492336b5bb
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:35:47 +0000

    Add new files and directories, commit changes, push changes

--- language/te/antiphishing_lang.php
+++ language/te/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'గేట్వే Antiphishing అనువర్తనం వెబ్, FTP, మెయిల్ మరియు మరిన్ని స్కాన్ ఒక కేంద్ర antiphishing ఇంజిన్ను ఉపయోగిస్తుంది. ఇది ఇంజన్ ద్వారా కనుగొనబడింది హానికరమైన లింకులు నుండి వినియోగదారులు రక్షణ కల్పించడం ద్వారా మీ నెట్వర్క్కు కనెక్ట్ పరికరాలు రక్షిస్తుంది.';
-$lang['antiphishing_app_name'] = 'గేట్వే Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'కూడా చాలా అవగాహన ఇంటర్నెట్ వినియోగదారులు అనుకోకుండా ఒక ఫిషింగ్ లింక్పై క్లిక్ చెయ్యండి. మేము ఎనేబుల్ అన్ని antiphishing లక్షణాలు వదిలి సిఫార్సు.';
-$lang['antiphishing_block_cloaked'] = 'బ్లాక్ మోసపూరిత URL లు';
-$lang['antiphishing_block_ssl_mismatch'] = 'బ్లాక్ SSL అసమతుల్యత';
-$lang['antiphishing_heuristics_engine'] = 'సమస్యా పరిష్కారం ఇంజిన్';
+$lang['antiphishing_app_description'] = 'గేట్ వే యాంటీ ఫిషింగ్ అనువర్తనం వెబ్, FTP, మెయిల్ మరియు మరిన్ని స్కాన్ చేయడానికి కేంద్ర యాంటీఫిషింగ్ ఇంజిన్ను ఉపయోగిస్తుంది. ఇది ఇంజిన్ ద్వారా కనుగొనబడిన హానికరమైన లింకుల నుండి వినియోగదారులను రక్షించడం ద్వారా మీ నెట్వర్క్కి కనెక్ట్ చేయబడిన పరికరాలను రక్షిస్తుంది.';
+$lang['antiphishing_app_name'] = 'గేట్ వే యాంటీ ఫిషింగ్';
+$lang['antiphishing_app_tooltip'] = 'అత్యంత అవగాహన ఇంటర్నెట్ వినియోగదారులు కూడా అనుకోకుండా ఫిషింగ్ లింక్పై క్లిక్ చేయవచ్చు. అన్ని అనారోగ్య ఫీచర్లను ఎనేబుల్ చెయ్యమని మేము సిఫార్సు చేస్తున్నాము.';
+$lang['antiphishing_block_cloaked'] = 'బ్లాక్ క్లోక్డ్ URL లు';
+$lang['antiphishing_block_ssl_mismatch'] = 'SSL అసమానతని బ్లాక్ చేయి';
+$lang['antiphishing_heuristics_engine'] = 'హ్యూరిస్టిక్ ఇంజిన్';
 $lang['antiphishing_signature_engine'] = 'సంతకం ఇంజిన్';
