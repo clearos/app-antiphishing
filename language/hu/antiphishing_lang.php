3bfb944fabf4dee2027625263b08d58faf32ba89
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:07:47 +0000

    Add new files and directories, commit changes, push changes

--- language/hu/antiphishing_lang.php
+++ language/hu/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'A Gateway Adathalászat app használ központi adathalászat motor olvasni web, FTP, e-mail és így tovább. Védi eszközök a hálózathoz csatlakoztatott árnyékolással felhasználókat a rosszindulatú hivatkozások által érzékelt motor.';
-$lang['antiphishing_app_name'] = 'Gateway adathalászat';
-$lang['antiphishing_app_tooltip'] = 'Még a legnagyobb hozzáértés internet felhasználók véletlenül kattintva egy adathalász linket. Javasoljuk, hogy hagyja az összes adathalászat funkciók engedélyezve.';
-$lang['antiphishing_block_cloaked'] = 'Blokk bújtatott URL';
-$lang['antiphishing_block_ssl_mismatch'] = 'Blokk SSL nem egyezik';
-$lang['antiphishing_heuristics_engine'] = 'heurisztikus motor';
-$lang['antiphishing_signature_engine'] = 'Signature Engine';
+$lang['antiphishing_app_description'] = 'A Gateway Antiphishing alkalmazás központi antiphishing motort használ a web, az FTP, az e-mail és egyebek vizsgálatához. Megvédi a hálózatához csatlakoztatott eszközöket a felhasználók által a motor által észlelt rosszindulatú kapcsolatokkal szemben.';
+$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Még a legelismertebb internetezők is véletlenül rákattinthatnak az adathalász-linkre. Azt javasoljuk, hogy hagyja abba az összes antiphishing funkciót.';
+$lang['antiphishing_block_cloaked'] = 'Blokkolt URL-ek blokkolása';
+$lang['antiphishing_block_ssl_mismatch'] = 'SSL-megegyezés blokkolása';
+$lang['antiphishing_heuristics_engine'] = 'Heurisztikus motor';
+$lang['antiphishing_signature_engine'] = 'Aláírási motor';
