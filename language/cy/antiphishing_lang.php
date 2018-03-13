60da1ff255d6d67a58611eeef23fc8e6370f6a6c
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:41:18 +0000

    Add new files and directories, commit changes, push changes

--- language/cy/antiphishing_lang.php
+++ language/cy/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'Mae&#39;r app Porth Antiphishing defnyddio peiriant antiphishing ganolog i sganio y we, FTP, post a mwy. Mae&#39;n amddiffyn dyfeisiau sy&#39;n gysylltiedig â&#39;ch rhwydwaith drwy gysgodi defnyddwyr o gysylltiadau maleisus a ganfuwyd gan y peiriant.';
-$lang['antiphishing_app_name'] = 'Porth Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'Gall hyd yn oed y defnyddwyr y Rhyngrwyd mwyaf savvy ddamweiniol glicio ar ddolen gwe-rwydo. Rydym yn argymell gadael holl nodweddion antiphishing alluogi.';
-$lang['antiphishing_block_cloaked'] = 'URLs Bloc y Sbriws';
-$lang['antiphishing_block_ssl_mismatch'] = 'Bloc SSL Camgymhariad';
-$lang['antiphishing_heuristics_engine'] = 'Engine hewristig';
-$lang['antiphishing_signature_engine'] = 'Engine Llofnod';
+$lang['antiphishing_app_description'] = 'Mae app Gateway Antiphishing yn defnyddio peiriant gwrthgyrru canolog i sganio gwe, FTP, post a mwy. Mae&#39;n gwarchod dyfeisiau sy&#39;n gysylltiedig â&#39;ch rhwydwaith trwy dynnu defnyddwyr o gysylltiadau maleisus a ddarganfyddir gan yr injan.';
+$lang['antiphishing_app_name'] = 'Rhwydo&#39;r Porth';
+$lang['antiphishing_app_tooltip'] = 'Gall hyd yn oed y defnyddwyr Rhyngrwyd mwyaf blasus glicio ar ddolen pysgota yn ddamweiniol. Rydym yn argymell gadael yr holl nodweddion gwrthgipio yn cael eu galluogi.';
+$lang['antiphishing_block_cloaked'] = 'URLau cloeon bloc';
+$lang['antiphishing_block_ssl_mismatch'] = 'Bloc SSL Mismatch';
+$lang['antiphishing_heuristics_engine'] = 'Peiriant Heuristig';
+$lang['antiphishing_signature_engine'] = 'Peiriant Llofnod';
