dfe772ea4725c5b6d9f03cfc73bc0ed1a429d6d6
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:21:53 +0000

    Add new files and directories, commit changes, push changes

--- language/ms/antiphishing_lang.php
+++ language/ms/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'Aplikasi Gateway Antiphishing menggunakan enjin antiphishing pusat untuk mengimbas web, FTP, mel dan banyak lagi. Ia melindungi peranti yang disambungkan ke rangkaian anda dengan melindungi pengguna daripada pautan berniat jahat dikesan oleh enjin.';
+$lang['antiphishing_app_description'] = 'Aplikasi Gateway Antiphishing menggunakan enjin antiphishing pusat untuk mengimbas web, FTP, mel dan banyak lagi. Ia melindungi peranti yang disambungkan ke rangkaian anda dengan melindungi pengguna dari pautan jahat yang dikesan oleh enjin.';
 $lang['antiphishing_app_name'] = 'Gateway Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'Malah pengguna Internet paling bijak sengaja boleh klik pada link phishing. Kami mengesyorkan meninggalkan semua ciri antiphishing yang aktif.';
-$lang['antiphishing_block_cloaked'] = 'Block diselaputi URL';
-$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL Sepadan';
-$lang['antiphishing_heuristics_engine'] = 'Engine heuristik';
-$lang['antiphishing_signature_engine'] = 'Signature Engine';
+$lang['antiphishing_app_tooltip'] = 'Malah pengguna Internet yang paling cerdas tidak boleh klik pada pautan phishing secara tidak sengaja. Kami mengesyorkan supaya semua ciri antiphishing diaktifkan.';
+$lang['antiphishing_block_cloaked'] = 'Blokkan Cloaked URLs';
+$lang['antiphishing_block_ssl_mismatch'] = 'Blok SSL Mismatch';
+$lang['antiphishing_heuristics_engine'] = 'Enjin Heuristik';
+$lang['antiphishing_signature_engine'] = 'Enjin Tandatangan';
