f06b36eb35b2891f3c6dd9da4422ca31f0889c06
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:33:14 +0000

    Add new files and directories, commit changes, push changes

--- language/su/antiphishing_lang.php
+++ language/su/antiphishing_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'Nu gateway Antiphishing app ngagunakeun mesin antiphishing puseur pikeun nyeken web, ftp, mail jeung leuwih. Eta ngajaga alat disambungkeun ka jaringan Anjeun ku shielding pamaké ti tutumbu jahat ditandaan ku mesin.';
+$lang['antiphishing_app_description'] = 'The gateway Antiphishing aplikasi ngagunakeun mesin antiphishing sentral pikeun nyeken web, ftp, mail sareng nu sanesna. Eta ngajaga alat disambungkeun ka jaringan anjeun ku shielding pamaké ti Tumbu jahat ditandaan ku mesin.';
 $lang['antiphishing_app_name'] = 'gateway Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'Komo pamaké Internet paling savvy ngahaja bisa klik dina kakait phishing. Urang nganjurkeun ninggalkeun sadaya fitur antiphishing diaktipkeun.';
+$lang['antiphishing_app_tooltip'] = 'Komo pamaké Internet paling savvy ngahaja tiasa klik dina link phishing. Urang nganjurkeun ninggalkeun sagala fitur antiphishing diaktipkeun.';
 $lang['antiphishing_block_cloaked'] = 'Blok Cloaked URL';
 $lang['antiphishing_block_ssl_mismatch'] = 'Blok SSL mismatch';
 $lang['antiphishing_heuristics_engine'] = 'Engine heuristik';
