d2eba2094f041812f0b54a49b2d4e58435699cce
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:37:29 +0000

    Add new files and directories, commit changes, push changes

--- language/tl/antiphishing_lang.php
+++ language/tl/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'The Gateway Antiphishing app ay gumagamit ng isang sentral na antiphishing engine upang i-scan web, FTP, mail at higit pa. Ito pinoprotektahan device na nakakonekta sa iyong network sa pamamagitan ng shielding ang mga gumagamit mula mapanghamak na link nakita ng mga engine.';
-$lang['antiphishing_app_name'] = 'gateway Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'Kahit na ang pinaka savvy mga gumagamit ng Internet ay maaaring sinasadyang mag-click sa isang phishing link. Inirerekomenda namin ang umaalis lahat ng mga tampok antiphishing pinagana.';
-$lang['antiphishing_block_cloaked'] = 'I-block ang cloaked URLs';
+$lang['antiphishing_app_description'] = 'Ang Gateway Antiphishing app ay gumagamit ng isang central antiphishing engine upang i-scan ang web, FTP, mail at iba pa. Pinoprotektahan nito ang mga device na nakakonekta sa iyong network sa pamamagitan ng shielding ng mga gumagamit mula sa mga nakakahamak na link na nakita ng engine.';
+$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Kahit na ang mga pinaka-savvy mga gumagamit ng Internet ay maaaring sinasadyang mag-click sa isang phishing link. Inirerekomenda naming iwanan ang lahat ng mga tampok na antiphishing.';
+$lang['antiphishing_block_cloaked'] = 'I-block ang Mga Cloaked na URL';
 $lang['antiphishing_block_ssl_mismatch'] = 'I-block ang SSL Mismatch';
-$lang['antiphishing_heuristics_engine'] = 'hyuristiko Engine';
-$lang['antiphishing_signature_engine'] = 'signature Engine';
+$lang['antiphishing_heuristics_engine'] = 'Heuristic Engine';
+$lang['antiphishing_signature_engine'] = 'Signature Engine';
