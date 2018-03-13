74690aabdd786369ab9d3fbb9f4da60573926d01
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:38:19 +0000

    Add new files and directories, commit changes, push changes

--- language/tr/antiphishing_lang.php
+++ language/tr/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'Ağ Geçidi Antiphishing uygulaması, web, FTP, mail ve daha taramak için merkezi bir antiphishing motoru kullanır. Bu motor tarafından tespit edilen kötü niyetli linkler kullanıcıları koruma ile ağınıza bağlı cihazların korur.';
-$lang['antiphishing_app_name'] = 'Ağ geçidi Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'Hatta en deneyimli internet kullanıcılarının yanlışlıkla bir phishing bağlantısını tıklayabilirsiniz. Biz etkinleştirilmiş tüm antiphishing özellikleri bırakmanızı öneririz.';
-$lang['antiphishing_block_cloaked'] = 'Blok Gizlenmiş URL&#39;ler';
-$lang['antiphishing_block_ssl_mismatch'] = 'Blok SSL Uyuşmazlığı';
+$lang['antiphishing_app_description'] = 'Gateway Antiphishing uygulaması web, FTP, mail ve daha fazlasını taramak için merkezi bir antiphishing motoru kullanıyor. Kullanıcıları, motor tarafından algılanan kötü amaçlı bağlantılardan korumak suretiyle ağınıza bağlı aygıtları korur.';
+$lang['antiphishing_app_name'] = 'Ağ Geçidi Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'En bilgili İnternet kullanıcıları bile yanlışlıkla bir kimlik avı bağlantısına tıklayabilir. Tüm antifishing özelliklerini etkin bırakmanızı tavsiye ederiz.';
+$lang['antiphishing_block_cloaked'] = 'Gizlenmiş URL&#39;leri Engelle';
+$lang['antiphishing_block_ssl_mismatch'] = 'SSL Eşleşmesini Engelle';
 $lang['antiphishing_heuristics_engine'] = 'Sezgisel Motor';
-$lang['antiphishing_signature_engine'] = 'İmza Motor';
+$lang['antiphishing_signature_engine'] = 'İmza Motoru';
