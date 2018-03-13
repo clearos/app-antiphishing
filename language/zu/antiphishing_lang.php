e9323acc3f87cc6505081ec52058ec7461049d12
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:43:26 +0000

    Add new files and directories, commit changes, push changes

--- language/zu/antiphishing_lang.php
+++ language/zu/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'Insiza Gateway Antiphishing lusebenzisa central antiphishing injini ukuskena web, FTP, mail nokuningi. Kuvikela amadivayisi kunethiwekhi yakho nge ukuzivikela abasebenzisi kusuka links enonya Kutsho injini.';
+$lang['antiphishing_app_description'] = 'Uhlelo lokusebenza lwe-Gateway Antiphishing lusebenzisa injini ephakathi yokulwa ne-antiphishing ukuskena iwebhu, i-FTP, i-mail nokuningi. Ivikela amadivayisi axhunyiwe kwinethiwekhi yakho ngokuvikela abasebenzisi kusuka ezixhumaneni ezinonya ezitholwe injini.';
 $lang['antiphishing_app_name'] = 'Gateway Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'Ngisho savvy kakhulu abasebenzisi Internet kungaba ngephutha uchofoze isixhumanisi iHluzo. Sincoma kusale zonke izici antiphishing inikwe amandla.';
-$lang['antiphishing_block_cloaked'] = 'Block Cloaked URLs';
-$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL Ukungafani';
-$lang['antiphishing_heuristics_engine'] = 'Engine yokuzifundela';
-$lang['antiphishing_signature_engine'] = 'Isiginesha Engine';
+$lang['antiphishing_app_tooltip'] = 'Ngisho nabasebenzisi be-intanethi abasebenzisa i-intanethi bangakwazi ukuchofoza ngengozi kusixhumanisi sokuphanga. Sincoma ukuthi zonke izici zokulwa nobugebengu zinikezwe amandla.';
+$lang['antiphishing_block_cloaked'] = 'Vimba ama-URL aqoshiwe';
+$lang['antiphishing_block_ssl_mismatch'] = 'Vimbela ukuxhuma kwe-SSL';
+$lang['antiphishing_heuristics_engine'] = 'I-Heuristic Engine';
+$lang['antiphishing_signature_engine'] = 'Injini Yokusayina';
