054de69252c62cb8868f83e501664218a2623c96
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:16:53 +0000

    Add new files and directories, commit changes, push changes

--- language/lt/antiphishing_lang.php
+++ language/lt/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'Vartai Kovotojai su nelegaliu programa naudoja centrinę Kovotojai su nelegaliu programą nuskaityti interneto, FTP, paštą ir daugiau. Jis apsaugo prietaisus prijungti prie tinklo ekranavimas vartotojus nuo kenkėjiškų nuorodų aptiktų variklio.';
-$lang['antiphishing_app_name'] = 'vartai Kovotojai su nelegaliu';
-$lang['antiphishing_app_tooltip'] = 'Net labiausiai patyrusių interneto vartotojų gali netyčia paspausti ant phishing nuorodą. Mes rekomenduojame palikti visas Kovotojai su nelegaliu funkcijų aktyvuota.';
-$lang['antiphishing_block_cloaked'] = 'Blokuoti Paslėptas URL';
-$lang['antiphishing_block_ssl_mismatch'] = 'Blokuoti SSL neatitikimas';
-$lang['antiphishing_heuristics_engine'] = 'euristinis Variklis';
-$lang['antiphishing_signature_engine'] = 'Parašas Variklis';
+$lang['antiphishing_app_description'] = '"Gateway Antiphishing" programa naudoja centrinį "antiphishing" variklį, kuris nuskaito žiniatinklį, FTP, el. Laišką ir kt. Jis apsaugo prietaisus, prijungtus prie jūsų tinklo, apsaugodamas vartotojus nuo variklio aptiktų kenkėjiškų nuorodų.';
+$lang['antiphishing_app_name'] = 'Vartai antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Net labiausiai išminties interneto vartotojai gali netyčia spustelėti spąstų nuorodą. Rekomenduojame palikti visas "antiphishing" funkcijas.';
+$lang['antiphishing_block_cloaked'] = 'Blokuoti apsaugoti URL';
+$lang['antiphishing_block_ssl_mismatch'] = 'Blokuoti SSL nesuderinamumą';
+$lang['antiphishing_heuristics_engine'] = 'Heistinis variklis';
+$lang['antiphishing_signature_engine'] = 'Parašo variklis';
