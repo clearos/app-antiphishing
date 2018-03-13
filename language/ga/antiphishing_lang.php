6d67422e5caabab11aeb1c319e60e739caceb658
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:09:15 +0000

    Add new files and directories, commit changes, push changes

--- language/ga/antiphishing_lang.php
+++ language/ga/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'Úsáideann an app Gateway AntiPhishing inneall AntiPhishing lárnach a scanadh gréasáin, FTP, post agus níos mó. Cosnaíonn sé feistí ceangailte le do líonra ag úsáideoirí ó naisc mailíseach deara ag an innill sciath.';
-$lang['antiphishing_app_name'] = 'Geata AntiPhishing';
-$lang['antiphishing_app_tooltip'] = 'Is féidir fiú na n-úsáideoirí Idirlín is savvy cliceáil thaisme ar nasc phishing. Molaimid ag fágáil na gnéithe AntiPhishing cumasaithe.';
-$lang['antiphishing_block_cloaked'] = 'Bloc cloaked URLanna';
+$lang['antiphishing_app_description'] = 'Úsáideann an t-inneall antiphishing Gateway inneall lárnach antiphishing chun scanadh gréasáin, FTP, ríomhphoist agus níos mó a scanadh. Cosnaíonn sé feistí a bhaineann le do líonra trí úsáideoirí ó naisc mailíseach a bhraitheann an t-inneall a sciúradh.';
+$lang['antiphishing_app_name'] = 'Tiomána Geata';
+$lang['antiphishing_app_tooltip'] = 'Is féidir leis na húsáideoirí Idirlín is fulangaí cliceáil ar thacaíocht phishing de thaisme. Molaimid fágáil na gnéithe antiphishing go léir a chumasú.';
+$lang['antiphishing_block_cloaked'] = 'URLanna Bloc Cloaked';
 $lang['antiphishing_block_ssl_mismatch'] = 'Bloc SSL Mismatch';
-$lang['antiphishing_heuristics_engine'] = 'Inneall heorastúla';
-$lang['antiphishing_signature_engine'] = 'Inneall síniú';
+$lang['antiphishing_heuristics_engine'] = 'Inneall Heuristic';
+$lang['antiphishing_signature_engine'] = 'Inneall Síniú';
