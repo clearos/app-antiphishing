02bbb6b0b61c7dd0279f0cf10145b2b4cabe540d
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:18:10 +0000

    Add new files and directories, commit changes, push changes

--- language/mg/antiphishing_lang.php
+++ language/mg/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'Ny Gateway Antiphishing fampiharana mampiasa ny foibe antiphishing maotera ny scan aterineto, FTP, namany Sary sy ny maro hafa. Tsy miaro ny fitaovana mifandray amin&#39;ny tambajotra amin&#39;ny alalan&#39;ny miaro mpampiasa avy mampidi-doza ny rohy hita ny maotera.';
-$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'Na dia ny tena savvy mpampiasa aterineto dia afaka nahy Tsindrio eo amin&#39;ny phishing rohy. Tsy fahazoan-dalana niala antiphishing rehetra endri-javatra alefa.';
-$lang['antiphishing_block_cloaked'] = 'Block Cloaked URLs';
-$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL Mismatch';
+$lang['antiphishing_app_description'] = 'Ny App Antiphishing an&#39;ny Gateway dia mampiasa môtera fanoherana afovoany mba hijerena tranonkala, FTP, mailaka ary bebe kokoa. Miaro ireo fitaovana mifandraika amin&#39;ny tambajotra amin&#39;ny alàlan&#39;ny fiarovana ny mpampiasa amin&#39;ny rohy maloto hitany avy amin&#39;ny motera.';
+$lang['antiphishing_app_name'] = 'Toriteny Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Na dia ireo mpampiasa aterineto tena mahay indrindra aza dia mety manindry amin&#39;ny rohy phishing. Manoro hevitra izahay ny hamela ireo endri-pihetseham-po rehetra alefa.';
+$lang['antiphishing_block_cloaked'] = 'Ambeno ny Cloaked URLs';
+$lang['antiphishing_block_ssl_mismatch'] = 'Tsindrio SSL';
 $lang['antiphishing_heuristics_engine'] = 'Heuristic Engine';
 $lang['antiphishing_signature_engine'] = 'Signature Engine';
