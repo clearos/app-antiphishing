81569a7766614daa012671d8588038905cc2a916
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:01:39 +0000

    Add new files and directories, commit changes, push changes

--- language/fi/antiphishing_lang.php
+++ language/fi/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'Gateway Suojaus tietojen kalastelulta app käyttää keskeinen antiphishing moottori skannata web, FTP, sähköposti ja paljon muuta. Se suojaa laitteita kytketty verkkoon suojaamalla käyttäjiä haitallisilta linkit havaitsee moottorin.';
-$lang['antiphishing_app_name'] = 'Gateway Suojaus tietojen kalastelulta';
-$lang['antiphishing_app_tooltip'] = 'Jopa kaikkein taju Internetin käyttäjät voivat vahingossa napsauttaa phishing linkkiä. Suosittelemme jättäen kaikki antiphishing ominaisuudet ovat käytössä.';
-$lang['antiphishing_block_cloaked'] = 'Block Peitetty URL-osoitteita';
-$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL täsmää';
-$lang['antiphishing_heuristics_engine'] = 'Heuristic Engine';
-$lang['antiphishing_signature_engine'] = 'Allekirjoitus Engine';
+$lang['antiphishing_app_description'] = 'Gateway Antiphishing -sovellus käyttää keskeistä antiphishing-moottoria Internetin, FTP: n, sähköpostin ja muiden tiedostojen skannaamiseen. Se suojaa verkkoon liitettyjä laitteita suojaamalla käyttäjiltä moottorin havaitsemia haittaohjelmia.';
+$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Viihdyttävät Internet-käyttäjät voivat vahingossa napsauttaa myös phishing-linkkiä. Suosittelemme, että kaikki antiphishing-toiminnot ovat käytössä.';
+$lang['antiphishing_block_cloaked'] = 'Block Cloaked URLs';
+$lang['antiphishing_block_ssl_mismatch'] = 'Estä SSL-yhteensopimattomuus';
+$lang['antiphishing_heuristics_engine'] = 'Heuristinen moottori';
+$lang['antiphishing_signature_engine'] = 'Allekirjoitusmoottori';
