bb6860361ed612a5b2920f992a70c9c26ec64621
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:24:50 +0000

    Add new files and directories, commit changes, push changes

--- language/pa/antiphishing_lang.php
+++ language/pa/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'ਗੇਟਵੇ Antiphishing ਐਪਲੀਕੇਸ਼ ਨੂੰ ਵੈੱਬ, FTP, ਮੇਲ ਅਤੇ ਹੋਰ ਨੂੰ ਸਕੈਨ ਕਰਨ ਲਈ ਇੱਕ ਮੱਧ antiphishing ਇੰਜਣ ਵਰਤਦਾ ਹੈ. ਇਹ ਇੰਜਣ ਦੁਆਰਾ ਖੋਜਿਆ ਖਤਰਨਾਕ ਲਿੰਕ ਤੱਕ ਉਪਭੋਗੀ ਨੂੰ ਬਚਾ ਕੇ ਆਪਣੇ ਨੈੱਟਵਰਕ ਨਾਲ ਜੁੜੇ ਜੰਤਰ ਦੀ ਰੱਖਿਆ ਕਰਦਾ ਹੈ.';
-$lang['antiphishing_app_name'] = 'ਗੇਟਵੇ Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'ਵੀ ਸਭ ਡਿਵੈਲਰਪਰ ਇੰਟਰਨੈੱਟ &#39;ਉਪਭੋਗੀ ਅਚਾਨਕ, ਇੱਕ ਫਿਸ਼ਿੰਗ ਲਿੰਕ&#39; ਤੇ ਕਲਿਕ ਕਰ ਸਕਦੇ ਹੋ. ਸਾਨੂੰ ਸਭ ਨੂੰ antiphishing ਫੀਚਰ ਨੂੰ ਯੋਗ ਨੂੰ ਛੱਡ ਕੇ ਸਿਫਾਰਸ਼.';
-$lang['antiphishing_block_cloaked'] = 'ਬਲਾਕ ਸਿੱਧ ਕੀਤੀ URL ਨੂੰ';
-$lang['antiphishing_block_ssl_mismatch'] = 'ਬਲਾਕ SSL ਨੂੰ ਮੇਲ';
-$lang['antiphishing_heuristics_engine'] = 'Heuristic ਇੰਜਣ';
+$lang['antiphishing_app_description'] = 'ਗੇਟਵੇ ਐਨਟੀਫਿਸ਼ਿੰਗ ਐਪ ਵੈੱਬ, FTP, ਮੇਲ ਅਤੇ ਹੋਰ ਬਹੁਤ ਕੁਝ ਸਕੈਨ ਕਰਨ ਲਈ ਸੈਂਟਰਲ ਐਂਟੀਫਿਸ਼ਿੰਗ ਇੰਜਨ ਦੀ ਵਰਤੋਂ ਕਰਦਾ ਹੈ. ਇਹ ਉਪਭੋਗਤਾ ਨੂੰ ਇੰਜਣ ਦੁਆਰਾ ਖੋਜੇ ਗਏ ਖਤਰਨਾਕ ਲਿੰਕ ਤੋਂ ਬਚਾ ਕੇ ਤੁਹਾਡੇ ਨੈਟਵਰਕ ਨਾਲ ਜੁੜੇ ਡਿਵਾਈਸਾਂ ਦੀ ਰੱਖਿਆ ਕਰਦਾ ਹੈ.';
+$lang['antiphishing_app_name'] = 'ਗੇਟਵੇ ਐਨਟੀਫਿਸ਼ਿੰਗ';
+$lang['antiphishing_app_tooltip'] = 'ਇੱਥੋਂ ਤੱਕ ਕਿ ਸਭ ਤੋਂ ਵੱਧ ਆਮਦਨੀ ਵਾਲੇ ਇੰਟਰਨੈਟ ਉਪਯੋਗਕਰਤਾ ਅਚਾਨਕ ਇੱਕ ਫਿਸ਼ਿੰਗ ਲਿੰਕ ਤੇ ਕਲਿਕ ਕਰ ਸਕਦੇ ਹਨ ਅਸੀਂ ਸਮਰਥਿਤ ਸਾਰੇ ਵਿਰੋਧੀ ਫੀਚਰਸ ਨੂੰ ਛੱਡਣ ਦੀ ਸਿਫਾਰਿਸ਼ ਕਰਦੇ ਹਾਂ';
+$lang['antiphishing_block_cloaked'] = 'ਬਲਾਕ ਕਲੋਕਡ ਯੂਆਰਐਲ';
+$lang['antiphishing_block_ssl_mismatch'] = 'ਬਲਾਕ SSL ਮਿਸਮੈਚ';
+$lang['antiphishing_heuristics_engine'] = 'ਅਨੁਕੂਲਨ ਇੰਜਨ';
 $lang['antiphishing_signature_engine'] = 'ਦਸਤਖਤ ਇੰਜਣ';
