71ea8210b2ac81de2f74dc0c97e358a6bf8734d6
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:24:24 +0000

    Add new files and directories, commit changes, push changes

--- language/no/antiphishing_lang.php
+++ language/no/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'The Gateway Antiphishing app bruker en sentral antiphishing-motor for å skanne web, FTP, e-post og mer. Det beskytter enheter som er koblet til nettverket ved skjerming brukere fra ondsinnede lenker oppdages av motoren.';
+$lang['antiphishing_app_description'] = 'Gateway Antiphishing-appen bruker en sentral antiphishing-motor for å skanne web, FTP, mail og mer. Det beskytter enheter som er koblet til nettverket ditt, ved å beskytte brukere mot ondsinnede koblinger registrert av motoren.';
 $lang['antiphishing_app_name'] = 'Gateway Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'Selv de mest erfarne Internett-brukere ved et uhell kan klikke på en phishing link. Vi anbefaler å la alle antiphishing funksjoner aktivert.';
-$lang['antiphishing_block_cloaked'] = 'Block Maskert webadresser';
-$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL Mismatch';
-$lang['antiphishing_heuristics_engine'] = 'heuristisk Engine';
-$lang['antiphishing_signature_engine'] = 'signatur Engine';
+$lang['antiphishing_app_tooltip'] = 'Selv de mest kyndige Internett-brukere kan ved et uhell klikke på en phishing-kobling. Vi anbefaler at du forlater alle antiphishing-funksjonene som er aktivert.';
+$lang['antiphishing_block_cloaked'] = 'Blokker klokkeadresser';
+$lang['antiphishing_block_ssl_mismatch'] = 'Blokker SSL-feil';
+$lang['antiphishing_heuristics_engine'] = 'Heuristisk motor';
+$lang['antiphishing_signature_engine'] = 'Signaturmotor';
