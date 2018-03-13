22a737d2edef78b980116a0492baaff1ff26429a
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:18:34 +0000

    Add new files and directories, commit changes, push changes

--- language/mi/antiphishing_lang.php
+++ language/mi/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'Ka whakamahia e te taupānga Gateway Antiphishing te pūkaha Antiphishing pokapū ki te matawai i tukutuku, FTP, mēra me te ake. tiaki te reira i whakaaro hono ki tou whatunga mā te paruru kaiwhakamahi i hononga kino kitea e te pūkaha.';
-$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'Ka taea e aitua ara nga kaiwhakamahi Ipurangi tino matika pāwhiri i runga i te hono hītinihanga. tūtohu tatou mahue āhuatanga Antiphishing katoa whakahohea.';
-$lang['antiphishing_block_cloaked'] = 'Poraka Rangatira URL';
-$lang['antiphishing_block_ssl_mismatch'] = 'Poraka SSL Koreōrite';
-$lang['antiphishing_heuristics_engine'] = 'Engine Heuristic';
-$lang['antiphishing_signature_engine'] = 'Engine waitohu';
+$lang['antiphishing_app_description'] = 'Ko te Gateway Antiphizing taupānga e whakamahi ana i te engine antiphishing pokapū ki te matawai i te tukutuku, FTP, meli me te ake. Ka tiakina e ia nga taputapu e hono ana ki to whatunga ma te tiaki i nga kaiwhakamahi mai i nga hononga kino e kitea ana e te engine.';
+$lang['antiphishing_app_name'] = 'Waahi Whakamutu';
+$lang['antiphishing_app_tooltip'] = 'Ahakoa ka taea e nga kaiwhakamahi Ipurangi ngawari te panui mo te hononga hītinihanga. Ka tūtohu kia waiho nga āhuatanga antiphishing katoa kua whakahohea.';
+$lang['antiphishing_block_cloaked'] = 'Poraka Ko nga URL ka';
+$lang['antiphishing_block_ssl_mismatch'] = 'Pouaka Aukati SSL';
+$lang['antiphishing_heuristics_engine'] = 'Heuristic Engine';
+$lang['antiphishing_signature_engine'] = 'Momo Waitohu';
