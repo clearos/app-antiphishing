6990cce12352b2993a57686e023b91aa7ce7f491
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:40:53 +0000

    Add new files and directories, commit changes, push changes

--- language/vi/antiphishing_lang.php
+++ language/vi/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'Ứng dụng Cổng Antiphishing sử dụng một động cơ chống giả mạo trung tâm để quét web, FTP, mail và nhiều hơn nữa. Nó bảo vệ các thiết bị kết nối vào mạng của bạn bằng cách chặn người dùng từ các liên kết độc hại được phát hiện bởi các công cụ.';
-$lang['antiphishing_app_name'] = 'cổng Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'Ngay cả những người sử dụng Internet hiểu biết nhất có thể vô tình bấm vào một liên kết lừa đảo. Chúng tôi khuyên bạn nên để lại tất cả các tính năng chống phishing được kích hoạt.';
-$lang['antiphishing_block_cloaked'] = 'Chặn URL Trùm kín';
-$lang['antiphishing_block_ssl_mismatch'] = 'Khối SSL không phù hợp';
-$lang['antiphishing_heuristics_engine'] = 'Động cơ Heuristic';
-$lang['antiphishing_signature_engine'] = 'Chữ ký cơ';
+$lang['antiphishing_app_description'] = 'Ứng dụng Gateway Antiphishing sử dụng công cụ antiphishing trung tâm để quét web, FTP, mail và nhiều hơn nữa. Nó bảo vệ các thiết bị kết nối với mạng của bạn bằng cách che chắn người dùng khỏi các liên kết độc hại được phát hiện bởi động cơ.';
+$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Ngay cả những người sử dụng Internet thông thái nhất cũng có thể vô tình nhấp vào liên kết lừa đảo. Chúng tôi khuyên bạn nên để tất cả các tính năng antiphishing được kích hoạt.';
+$lang['antiphishing_block_cloaked'] = 'Chặn URL bị Cloaked';
+$lang['antiphishing_block_ssl_mismatch'] = 'Chặn không phù hợp SSL';
+$lang['antiphishing_heuristics_engine'] = 'Công cụ Heuristic';
+$lang['antiphishing_signature_engine'] = 'Chữ ký Engine';
