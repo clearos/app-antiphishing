f93ca9fe07bf3a92d5ce9fdc6841df1ff1033690
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:33:39 +0000

    Add new files and directories, commit changes, push changes

--- language/sw/antiphishing_lang.php
+++ language/sw/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'programu Gateway AntiPhishing anatumia kati AntiPhishing injini Scan mtandao, FTP, barua pepe na zaidi. Ni kulinda vifaa kushikamana na mtandao wako kwa kuzuia watumiaji kutoka viungo malicious wanaona na injini.';
-$lang['antiphishing_app_name'] = 'Gateway AntiPhishing';
-$lang['antiphishing_app_tooltip'] = 'Hata watumiaji wengi savvy Internet unaweza click ajali juu ya kiungo hadaa. Sisi kupendekeza kuacha makala yote AntiPhishing kuwezeshwa.';
-$lang['antiphishing_block_cloaked'] = 'Block cloaked URLs';
-$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL Mismatch';
-$lang['antiphishing_heuristics_engine'] = 'heuristic injini za';
-$lang['antiphishing_signature_engine'] = 'Signature injini za';
+$lang['antiphishing_app_description'] = 'Programu ya Antiphishing ya programu hutumia injini ya kupambana na kufuta mtandao, FTP, barua na zaidi. Inalinda vifaa vinavyounganishwa na mtandao wako kwa kuzuia watumiaji kutoka viungo vibaya vinavyotambuliwa na injini.';
+$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Hata watumiaji wengi wa wavuti wanaoweza kuingia kwenye Intaneti wanaweza kubofya kiungo kwenye kiungo cha ulaghai. Tunapendekeza kuondoka vipengele vyote vya kupambana na uharibifu vimewezeshwa.';
+$lang['antiphishing_block_cloaked'] = 'Zuia URL zilizopigwa';
+$lang['antiphishing_block_ssl_mismatch'] = 'Zuia Machapisho ya SSL';
+$lang['antiphishing_heuristics_engine'] = 'Injini ya Heuristic';
+$lang['antiphishing_signature_engine'] = 'Injini ya Saini';
