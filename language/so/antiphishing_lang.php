cb245730d930aaa40117d4026f8f39592322c118
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:31:06 +0000

    Add new files and directories, commit changes, push changes

--- language/so/antiphishing_lang.php
+++ language/so/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'The app Gateway Antiphishing isticmaalaa engine a antiphishing dhexe in iskaan web, FTP, mail oo dheeraad ah. Wuxuu ka hortagaa qalabka ku xiran aad network by gaashaamashada users ka links xaasidnimo ah la ogaado by engine ah.';
-$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'Xataa dadka isticmaala Internet ugu bixio filaan ah riixi kartaa link phishing ah. Waxaan ku talineynaa tago oo dhan muuqaalada antiphishing karti.';
-$lang['antiphishing_block_cloaked'] = 'Block Cloaked URLs';
-$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL jaanqaadayn';
-$lang['antiphishing_heuristics_engine'] = 'Engine Heuristic';
-$lang['antiphishing_signature_engine'] = 'Saxiixa Engine';
+$lang['antiphishing_app_description'] = 'Barnaamijka "Antivirus" ee Gateway wuxuu isticmaalaa mashiinka dhexe ee wax lagu buufiyo si uu u-eego webka, FTP, boostada iyo wixii intaa ka badan. Waxay ilaalisaa qalabyada ku xiran shabakadaada adigoo kaftamaya dadka isticmaala xariiro xaasidnimo ah oo lagu ogaaday mishiinka.';
+$lang['antiphishing_app_name'] = 'Gawaarida Kufsiga';
+$lang['antiphishing_app_tooltip'] = 'Xitaa dadka isticmaala Internetka ugu khatarsan ayaa si khalad ah u riixaya xiriirka phishing. Waxaan kugula talineynaa inaad ka tagto dhamaan sifooyinka ka hortagga.';
+$lang['antiphishing_block_cloaked'] = 'Gali URL-yada la xirxiro';
+$lang['antiphishing_block_ssl_mismatch'] = 'Xakamaynta SSL Isafgarad';
+$lang['antiphishing_heuristics_engine'] = 'Engine Maqalka';
+$lang['antiphishing_signature_engine'] = 'Saxeexa Saxeexa';
