a209cd3870d98c6b0cb948bfcd93cf31ae654c2f
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:31:57 +0000

    Add new files and directories, commit changes, push changes

--- language/st/antiphishing_lang.php
+++ language/st/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'The app e heke Antiphishing sebelisa bohareng antiphishing enjene go Scan web, FTP, mangolo le ho feta. E sireletsa disebediswa tse amanang le ho hokahanngoa hao ka sireletsoe basebedisi ho tloha Les liens de le nang le lonya lemoha ka enjene.';
-$lang['antiphishing_app_name'] = 'heke Antiphishing';
-$lang['antiphishing_app_tooltip'] = 'Esita le basebedisi fetisisa savvy Internet ka tobetsa ka phoso mabapi le lehokela phishing. Re ho kgothaletsa siea litšobotsi tsohle antiphishing nolofalletsa.';
-$lang['antiphishing_block_cloaked'] = 'Block Cloaked URLs';
-$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL Mismatch';
+$lang['antiphishing_app_description'] = 'Lenaneo la Gateway Antiphishing le sebelisa mochine o ka sehloohong oa ho loantša li-web, FTP, mail le ho feta. E sireletsa lisebelisoa tse amanang le marang-rang a hao ka ho sireletsa basebelisi ba li-link tse kotsi tse fumanoang ke enjene.';
+$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Esita le basebelisi ba Inthanete ba nang le mekhoa e metle ka ho fetisisa ba ka penya konopo ea phishing. Re khothaletsa ho tlohela likarolo tsohle tsa antiphishing li nolofalitsoe.';
+$lang['antiphishing_block_cloaked'] = 'Tlosa URL ea Cloaked';
+$lang['antiphishing_block_ssl_mismatch'] = 'Tlosa SSL ho se thibe';
 $lang['antiphishing_heuristics_engine'] = 'Heuristic Engine';
-$lang['antiphishing_signature_engine'] = 'Signature Engine';
+$lang['antiphishing_signature_engine'] = 'Engine Engine';
