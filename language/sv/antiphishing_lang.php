75560c8dfdc7e8e4d7c099a27454c667e6670186
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 00:34:31 +0000

    Add new files and directories, commit changes, push changes

--- language/sv/antiphishing_lang.php
+++ language/sv/antiphishing_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['antiphishing_app_description'] = 'Gateway nätfiske app använder en central nätfiske motor för att skanna webben, FTP, e-post och mer. Det skyddar enheter som är anslutna till nätverket genom avskärmning användare från skadliga länkar som upptäckts av motorn.';
-$lang['antiphishing_app_name'] = 'gateway nätfiske';
-$lang['antiphishing_app_tooltip'] = 'Även de mest kunniga Internetanvändare kan av misstag klicka på en nätfiskelänk. Vi rekommenderar att du lämnar alla nätfiske funktioner aktiverade.';
-$lang['antiphishing_block_cloaked'] = 'Block Cloaked adresser';
-$lang['antiphishing_block_ssl_mismatch'] = 'Block SSL Mismatch';
-$lang['antiphishing_heuristics_engine'] = 'heuristisk motor';
-$lang['antiphishing_signature_engine'] = 'signatur Engine';
+$lang['antiphishing_app_description'] = 'Gateway Antiphishing-appen använder en central antiphishing-motor för att skanna webb, FTP, mail och mer. Det skyddar enheter som är anslutna till ditt nätverk genom att skydda användare från skadliga länkar som upptäckts av motorn.';
+$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
+$lang['antiphishing_app_tooltip'] = 'Även de mest kunniga Internetanvändarna kan av misstag klicka på en phishing-länk. Vi rekommenderar att du lämnar alla antiphishing-funktioner aktiverade.';
+$lang['antiphishing_block_cloaked'] = 'Blockera urklippta webbadresser';
+$lang['antiphishing_block_ssl_mismatch'] = 'Blockera SSL-matchning';
+$lang['antiphishing_heuristics_engine'] = 'Heuristisk motor';
+$lang['antiphishing_signature_engine'] = 'Signatur Engine';
